7319b32675478aed8945cc3561ee88c62702bf2c
Yoshihiro Yamazaki <yoya@awm.jp>
2017-03-03 20:01:10 +0900

    split function to phase1, phase2, phase3.

--- fcbi.js
+++ fcbi.js
@@ -115,6 +115,23 @@ function drawFCBI() {
     var srcData = srcImageData.data;
     var dstData = dstImageData.data;
     // リサンプル
+    drawFCBI_Phase1(srcImageData, dstImageData);
+
+    if (phase > 1) {
+	// 対角成分補間
+	drawFCBI_Phase2(dstImageData, TM, edge)
+    } if (phase > 2) {
+	// 水平垂直成分補完
+	drawFCBI_Phase3(dstImageData, TM, edge)
+    }
+    dstCtx.putImageData(dstImageData, 0, 0);
+}
+
+/*
+ *  リサンプル(補間なし)
+ */
+function drawFCBI_Phase1(srcImageData, dstImageData) {
+    var dstWidth = dstImageData.width, dstHeight = dstImageData.height;
     for (var dstY = 0 ; dstY < dstHeight; dstY+=2) {
         for (var dstX = 0 ; dstX < dstWidth; dstX+=2) {
 	    var srcX = dstX/2;
@@ -123,122 +140,130 @@ function drawFCBI() {
 	    setRGBA(dstImageData, dstX, dstY, rgba);
 	}
     }
-    if (phase > 1) {
-	// 対角成分
-	for (var dstY = 1 ; dstY < dstHeight; dstY+=2) {
-            for (var dstX = 1 ; dstX < dstWidth; dstX+=2) {
-		/*  l1     l2
-		 *      x  
-		 *  l3     l4
-		 */
-		var l1 = getLuma(dstImageData, dstX-1, dstY-1);
-		var l2 = getLuma(dstImageData, dstX+1, dstY-1);
-		var l3 = getLuma(dstImageData, dstX-1, dstY+1);
-		var l4 = getLuma(dstImageData, dstX+1, dstY+1);
-		var V1 = Math.abs(l1 - l4);
-		var V2 = Math.abs(l2 - l3);
-		var p1 = (l1 + l4) / 2;
-		var p2 = (l2 + l3) / 2;
-		if ((V1 < TM) && (V2 < TM) && (Math.abs(p1 - p2) < TM)) {
-		    if (edge) {
-			var rgba = [255, 0, 0, 255];
-		    } else {
-			var H1 = FilterMultiply(dstImageData, dstX, dstY,
-						[[-3, 1],[-1, -1], [1, -3], // 1,2,3
-						 [-1, 1], [1, -1],          // 4, 5
-						 [-1, 3], [1,1], [3, -1]],  // 6, 7, 8
-						[1, 1, 1, -3, -3, 1, 1, 1]); // filter
-			var H2 = FilterMultiply(dstImageData, dstX, dstY,
-						[[-1, -3],[1, 1], [3, 1],   // 1,2,3
-						 [-1, -1], [1, 1],          // 4, 5
-						 [-3, -1], [-1,1], [1, 3]], // 6, 7, 8
-						[1, 1, 1, -3, -3, 1, 1, 1]); // filter
-			if (H1 < H2) {
-			    var rgba1 = getRGBA(dstImageData, dstX-1, dstY-1);
-			    var rgba4 = getRGBA(dstImageData, dstX+1, dstY+1);
-			    var rgba = meanRGBA(rgba1, rgba4);
-			} else {
-			    var rgba2 = getRGBA(dstImageData, dstX-1, dstY+1);
-			    var rgba3 = getRGBA(dstImageData, dstX+1, dstY-1);
-			    var rgba = meanRGBA(rgba2, rgba3);
-			}
-		    }
+}
+
+/*
+ * 対角成分補間
+ */
+function drawFCBI_Phase2(dstImageData, TM, edge) {
+    var dstWidth = dstImageData.width, dstHeight = dstImageData.height;
+    for (var dstY = 1 ; dstY < dstHeight; dstY+=2) {
+        for (var dstX = 1 ; dstX < dstWidth; dstX+=2) {
+	    /*  l1     l2
+	     *      x  
+	     *  l3     l4
+	     */
+	    var l1 = getLuma(dstImageData, dstX-1, dstY-1);
+	    var l2 = getLuma(dstImageData, dstX+1, dstY-1);
+	    var l3 = getLuma(dstImageData, dstX-1, dstY+1);
+	    var l4 = getLuma(dstImageData, dstX+1, dstY+1);
+	    var V1 = Math.abs(l1 - l4);
+	    var V2 = Math.abs(l2 - l3);
+	    var p1 = (l1 + l4) / 2;
+	    var p2 = (l2 + l3) / 2;
+	    if ((V1 < TM) && (V2 < TM) && (Math.abs(p1 - p2) < TM)) {
+		if (edge) {
+		    var rgba = [255, 0, 0, 255];
 		} else {
-		    if (edge) {
-			var rgba = [0, 255, 0, 255];
+		    var H1 = FilterMultiply(dstImageData, dstX, dstY,
+					    [[-3, 1],[-1, -1], [1, -3], // 1,2,3
+					     [-1, 1], [1, -1],          // 4, 5
+					     [-1, 3], [1,1], [3, -1]],  // 6, 7, 8
+					    [1, 1, 1, -3, -3, 1, 1, 1]); // filter
+		    var H2 = FilterMultiply(dstImageData, dstX, dstY,
+					    [[-1, -3],[1, 1], [3, 1],   // 1,2,3
+					     [-1, -1], [1, 1],          // 4, 5
+					     [-3, -1], [-1,1], [1, 3]], // 6, 7, 8
+					    [1, 1, 1, -3, -3, 1, 1, 1]); // filter
+		    if (H1 < H2) {
+			var rgba1 = getRGBA(dstImageData, dstX-1, dstY-1);
+			var rgba4 = getRGBA(dstImageData, dstX+1, dstY+1);
+			var rgba = meanRGBA(rgba1, rgba4);
 		    } else {
-			if (V1 < V2) {
-			    var rgba1 = getRGBA(dstImageData, dstX-1, dstY-1);
-			    var rgba4 = getRGBA(dstImageData, dstX+1, dstY+1);
-			    var rgba = meanRGBA(rgba1, rgba4);
-			} else{
-			    var rgba2 = getRGBA(dstImageData, dstX-1, dstY+1);
-			    var rgba3 = getRGBA(dstImageData, dstX+1, dstY-1);
-			    var rgba = meanRGBA(rgba2, rgba3);
-			}
+			var rgba2 = getRGBA(dstImageData, dstX-1, dstY+1);
+			var rgba3 = getRGBA(dstImageData, dstX+1, dstY-1);
+			var rgba = meanRGBA(rgba2, rgba3);
 		    }
 		}
-		setRGBA(dstImageData, dstX, dstY, rgba);
-	    }
-	}
-    } if (phase > 2) {
-	// 水平垂直成分
-	for (var dstY = 0 ; dstY < dstHeight; dstY++) {
-            for (var dstX = 1 - (dstY%2) ; dstX < dstWidth; dstX+=2) {
-		/*     l2
-		 *  l1  x  l4
-		 *     l3
-		 */
-		var l1 = getLuma(dstImageData, dstX-1, dstY);
-		var l2 = getLuma(dstImageData, dstX, dstY-1);
-		var l3 = getLuma(dstImageData, dstX, dstY+1);
-		var l4 = getLuma(dstImageData, dstX+1, dstY);
-		var V1 = Math.abs(l1 - l4);
-		var V2 = Math.abs(l2 - l3);
-		var p1 = (l1 + l4) / 2;
-		var p2 = (l2 + l3) / 2;
-		if ((V1 < TM) && (V2 < TM) && (Math.abs(p1 - p2) < TM)) {
-		    if (edge) {
-			var rgba = [0, 0, 255, 255];
-		    } else {
-			var H1 = FilterMultiply(dstImageData, dstX, dstY,
-						[[1, -2],[1, 0], [1, 2],     // 1,2,3
-						 [0, -1], [0, 1],            // 4, 5
-						 [-1, -2], [-1,0], [-1, 2]], // 6, 7, 8
-						[1, 1, 1, -3, -3, 1, 1, 1]); // filter
-			var H2 = FilterMultiply(dstImageData, dstX, dstY,
-						[[-2, -1],[0, -1], [2, -1],  // 1,2,3
-						 [-1, 0], [1, 0],          // 4, 5
-						 [-2, 1], [0,1], [2, 1]], // 6, 7, 8
-						[1, 1, 1, -3, -3, 1, 1, 1]); // filter
-			if (H1 < H2) {
-			    var rgba2 = getRGBA(dstImageData, dstX, dstY-1);
-			    var rgba3 = getRGBA(dstImageData, dstX, dstY+1);
-			    var rgba = meanRGBA(rgba2, rgba3);
-			} else {
-			    var rgba1 = getRGBA(dstImageData, dstX-1, dstY);
-			    var rgba4 = getRGBA(dstImageData, dstX+1, dstY);
-			    var rgba = meanRGBA(rgba1, rgba4);
-			}
-		    }
+	    } else {
+		if (edge) {
+		    var rgba = [0, 255, 0, 255];
 		} else {
-		    if (edge) {
-	    		var rgba = [255, 255, 0, 255];
-		    } else {
-			if (V1 < V2) {
-			    var rgba1 = getRGBA(dstImageData, dstX-1, dstY);
-			    var rgba4 = getRGBA(dstImageData, dstX+1, dstY);
-			    var rgba = meanRGBA(rgba1, rgba4);
-			} else{
-			    var rgba2 = getRGBA(dstImageData, dstX, dstY-1);
-			    var rgba3 = getRGBA(dstImageData, dstX, dstY+1);
-			    var rgba = meanRGBA(rgba2, rgba3);
-			}
+		    if (V1 < V2) {
+			var rgba1 = getRGBA(dstImageData, dstX-1, dstY-1);
+			var rgba4 = getRGBA(dstImageData, dstX+1, dstY+1);
+			var rgba = meanRGBA(rgba1, rgba4);
+		    } else{
+			var rgba2 = getRGBA(dstImageData, dstX-1, dstY+1);
+			var rgba3 = getRGBA(dstImageData, dstX+1, dstY-1);
+			var rgba = meanRGBA(rgba2, rgba3);
 		    }
 		}
-		setRGBA(dstImageData, dstX, dstY, rgba);
 	    }
+	    setRGBA(dstImageData, dstX, dstY, rgba);
 	}
     }
-    dstCtx.putImageData(dstImageData, 0, 0);
+}
+
+/*
+ * 水平垂直成分補完
+ */
+function drawFCBI_Phase3(dstImageData, TM, edge) {
+    var dstWidth = dstImageData.width, dstHeight = dstImageData.height;
+    for (var dstY = 0 ; dstY < dstHeight; dstY++) {
+        for (var dstX = 1 - (dstY%2) ; dstX < dstWidth; dstX+=2) {
+	    /*     l2
+	     *  l1  x  l4
+	     *     l3
+	     */
+	    var l1 = getLuma(dstImageData, dstX-1, dstY);
+	    var l2 = getLuma(dstImageData, dstX, dstY-1);
+	    var l3 = getLuma(dstImageData, dstX, dstY+1);
+	    var l4 = getLuma(dstImageData, dstX+1, dstY);
+	    var V1 = Math.abs(l1 - l4);
+	    var V2 = Math.abs(l2 - l3);
+	    var p1 = (l1 + l4) / 2;
+	    var p2 = (l2 + l3) / 2;
+	    if ((V1 < TM) && (V2 < TM) && (Math.abs(p1 - p2) < TM)) {
+		if (edge) {
+		    var rgba = [0, 0, 255, 255];
+		} else {
+		    var H1 = FilterMultiply(dstImageData, dstX, dstY,
+					    [[1, -2],[1, 0], [1, 2],     // 1,2,3
+					     [0, -1], [0, 1],            // 4, 5
+					     [-1, -2], [-1,0], [-1, 2]], // 6, 7, 8
+					    [1, 1, 1, -3, -3, 1, 1, 1]); // filter
+		    var H2 = FilterMultiply(dstImageData, dstX, dstY,
+					    [[-2, -1],[0, -1], [2, -1],  // 1,2,3
+					     [-1, 0], [1, 0],          // 4, 5
+					     [-2, 1], [0,1], [2, 1]], // 6, 7, 8
+					    [1, 1, 1, -3, -3, 1, 1, 1]); // filter
+		    if (H1 < H2) {
+			var rgba2 = getRGBA(dstImageData, dstX, dstY-1);
+			var rgba3 = getRGBA(dstImageData, dstX, dstY+1);
+			var rgba = meanRGBA(rgba2, rgba3);
+		    } else {
+			var rgba1 = getRGBA(dstImageData, dstX-1, dstY);
+			var rgba4 = getRGBA(dstImageData, dstX+1, dstY);
+			var rgba = meanRGBA(rgba1, rgba4);
+		    }
+		}
+	    } else {
+		if (edge) {
+	    	    var rgba = [255, 255, 0, 255];
+		} else {
+		    if (V1 < V2) {
+			var rgba1 = getRGBA(dstImageData, dstX-1, dstY);
+			var rgba4 = getRGBA(dstImageData, dstX+1, dstY);
+			var rgba = meanRGBA(rgba1, rgba4);
+		    } else{
+			var rgba2 = getRGBA(dstImageData, dstX, dstY-1);
+			var rgba3 = getRGBA(dstImageData, dstX, dstY+1);
+			var rgba = meanRGBA(rgba2, rgba3);
+		    }
+		}
+	    }
+	    setRGBA(dstImageData, dstX, dstY, rgba);
+	}
+    }
 }
