6a7183df20404870198f4924546ee46118d93bbd
Yoshihiro Yamazaki <yoya@awm.jp>
2017-04-24 21:48:40 +0900

    level(dynamic range) adjustment.

--- histogram.js
+++ histogram.js
@@ -14,6 +14,10 @@ function main() {
     var srcHistCanvas = document.getElementById("srcHistCanvas");
     var dstHistCanvas = document.getElementById("dstHistCanvas");
     var srcImage = new Image(srcCanvas.width, srcCanvas.height);
+    var maxValueRange = document.getElementById("maxValueRange");
+    var minValueRange = document.getElementById("minValueRange");
+    var maxValueText = document.getElementById("maxValueText");
+    var minValueText = document.getElementById("minValueText");
     dropFunction(document, function(dataURL) {
 	srcImage = new Image();
 	srcImage.onload = function() {
@@ -22,8 +26,22 @@ function main() {
 	srcImage.src = dataURL;
     }, "DataURL");
     bindFunction({"maxWidthHeightRange":"maxWidthHeightText",
-		  "equalizeCheckbox":null},
-		 function() {
+		  "equalizeCheckbox":null,
+		  "maxValueRange":"maxValueText",
+		  "minValueRange":"minValueText"},
+		 function(target) {
+		     console.debug(target.id);
+		     var maxValue = parseFloat(document.getElementById("maxValueRange").value);
+		     var minValue = parseFloat(document.getElementById("minValueRange").value);
+		     if (minValue > maxValue) {
+			 if ((target.id === "maxValueRange") || (target.id === "maxValueText")) {
+			     minValueRange.value = maxValue;
+			     minValueText.value  = maxValue;
+			 } else {
+			     maxValueRange.value = minValue;
+			     maxValueText.value  = minValue;
+			 }
+		     }
 		     drawSrcImageAndHistogram(srcImage, srcCanvas, dstCanvas, srcHistCanvas, dstHistCanvas);
 		 } );
 }
@@ -31,11 +49,14 @@ function main() {
 function drawSrcImageAndHistogram(srcImage, srcCanvas, dstCancas, srcHistCanvas, dstHistCanvas) {
     var maxWidthHeight = parseFloat(document.getElementById("maxWidthHeightRange").value);
     var equalize = document.getElementById("equalizeCheckbox").checked;
+    var maxValue = parseFloat(document.getElementById("maxValueRange").value);
+    var minValue = parseFloat(document.getElementById("minValueRange").value);
     drawSrcImage(srcImage, srcCanvas, maxWidthHeight);
-    drawHistogram(srcCanvas, dstCanvas, srcHistCanvas, dstHistCanvas, equalize);
+    drawHistogram(srcCanvas, dstCanvas, srcHistCanvas, dstHistCanvas, equalize, minValue, maxValue);
 }
 
-function drawHistgramGraph(histCanvas, redHist, greenHist, blueHist) {
+function drawHistgramGraph(histCanvas, redHist, greenHist, blueHist,
+			   minValue, maxValue) {
     var height = histCanvas.height;
     histCanvas.style.backgroundColor = "black";
     histCanvas.height = height; // canvas clear
@@ -66,21 +87,40 @@ function drawHistgramGraph(histCanvas, redHist, greenHist, blueHist) {
 	    ctx.stroke();
 	}
     }
+    ctx.fillStyle="gray";
+    if (0 < minValue) {
+	ctx.beginPath();
+	ctx.moveTo(0, 0);
+	ctx.rect(0, 0, minValue, height);
+	ctx.fill();
+    }
+    if (maxValue < 255) {
+ 	ctx.beginPath();
+	ctx.moveTo(maxValue+1, 0);
+	ctx.rect(maxValue+1, 0, 256, height);
+	ctx.fill();
+    }
 }
 
-function equalizeMap(redHist, greenHist, blueHist) {
+function equalizeMap(redHist, greenHist, blueHist, minValue, maxValue) {
     var map = new Uint8Array(256);
-    var nColors = redHist.reduce( function(prev, cur) { return prev + cur; });
-    nColors += greenHist.reduce( function(prev, cur) { return prev + cur; });
-    nColors += blueHist.reduce( function(prev, cur) { return prev + cur; });
+    var reduceFnSum = function(prev, cur) {
+	if ((cur < minValue) || (maxValue < cur)) {
+	    return prev;
+	}
+	return prev + cur;
+    };
+    var nColors = redHist.reduce(reduceFnSum);
+    nColors += greenHist.reduce(reduceFnSum);
+    nColors += blueHist.reduce(reduceFnSum);
     var count = 0;
     for (var i = 0; i < 256 ; i++) {
 	var c = redHist[i] + greenHist[i] + blueHist[i];
 	if (c > 0) {
 	    count += c;
-	    map[i] = 255;
+	    map[i] = maxValue;
 	    for (var j = 0; j < 256 ; j++) {
-		if (count <= (nColors / 256 * (j+1))) {
+		if (count <= (nColors / maxValue * (j+1))) {
 		    map[i] = j;
 		    break;
 		}
@@ -90,8 +130,7 @@ function equalizeMap(redHist, greenHist, blueHist) {
     return map;
 }
 
-
-function drawHistogram(srcCanvas, dstCanvas, srcHistCanvas, dstHistCanvas, equalize) {
+function drawHistogram(srcCanvas, dstCanvas, srcHistCanvas, dstHistCanvas, equalize, minValue, maxValue) {
     // console.debug("drawHistogram");
     var srcCtx = srcCanvas.getContext("2d");
     var dstCtx = dstCanvas.getContext("2d");
@@ -106,9 +145,18 @@ function drawHistogram(srcCanvas, dstCanvas, srcHistCanvas, dstHistCanvas, equal
     var redHist   = getColorHistogramList(srcCanvas, "red");
     var greenHist = getColorHistogramList(srcCanvas, "green");
     var blueHist  = getColorHistogramList(srcCanvas, "blue");
-    drawHistgramGraph(srcHistCanvas, redHist, greenHist, blueHist);
+    drawHistgramGraph(srcHistCanvas, redHist, greenHist, blueHist, minValue, maxValue);
     if (equalize) {
 	var colorMap = equalizeMap(redHist, greenHist, blueHist);
+    } else if (( 0 < minValue) || (maxValue< 255)) {
+	var colorMap = new Uint8Array(256).map(function(n, i) {
+	    if (i < minValue) {
+		return 0;
+	    } else if (maxValue < i) {
+		return 255;
+	    }
+	    return 255 * (i - minValue) / (maxValue - minValue);
+	});
     } else {
 	var colorMap = new Uint8Array(256).map(function(n, i) { return i; });
     }
@@ -126,5 +174,5 @@ function drawHistogram(srcCanvas, dstCanvas, srcHistCanvas, dstHistCanvas, equal
     var redHist   = getColorHistogramList(dstCanvas, "red");
     var greenHist = getColorHistogramList(dstCanvas, "green");
     var blueHist  = getColorHistogramList(dstCanvas, "blue");
-    drawHistgramGraph(dstHistCanvas, redHist, greenHist, blueHist);
+    drawHistgramGraph(dstHistCanvas, redHist, greenHist, blueHist, 0, 255);
 }
