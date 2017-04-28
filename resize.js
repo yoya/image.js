3ca7ec846938db37117545b32e794aff5d46ed2c
Yoshihiro Yamazaki <yoya@awm.jp>
2017-04-28 22:08:53 +0900

    fix resize routine

--- resize.js
+++ resize.js
@@ -6,60 +6,44 @@
 
 main();
 
-function getById(id) { return document.getElementById(id); }
-
 function main() {
-    var selectBindingList = {"filterType":null};
-    var rangeBindingList  = {"logScale":"log",
-			     "cubicB":"direct", "cubicC":"direct",
-			     "lobe":"direct"};
-    var valueTable = {};
-    for (var id in selectBindingList) {
-	var type = selectBindingList[id];
-	selectBinding(valueTable, id);
-	valueTable[id] = getById(id).value;
-    }
-    for (var id in rangeBindingList) {
-	var type = rangeBindingList[id];
-	rangeBinding(valueTable, id, type);
-	var value = getById(id+"Value").value;
-	valueTable[id] = value;
-    }
-    imageBinding(valueTable);
-    update(valueTable, true);
-}
-
-function update(valueTable, heavy) {
-    drawGraph(valueTable);
-    if (heavy) {
-	var srcCanvas = getById("srcCanvas");
-	var dstCanvas = getById("dstCanvas");
-	resizeFunc(srcCanvas, dstCanvas, valueTable);
-    }
-}
-
-function imageBinding(valueTable) {
-    var srcCanvas = getById("srcCanvas");
-    var srcCtx = srcCanvas.getContext("2d");
-    var dstCanvas = getById("dstCanvas");
+    // console.debug("main");
+    var srcCanvas = document.getElementById("srcCanvas");
+    var dstCanvas = document.getElementById("dstCanvas");
+    var srcImage = new Image(srcCanvas.width, srcCanvas.height);
     dropFunction(document, function(dataURL) {
-	if (dataURL === null) {
-	    return ;
+	srcImage = new Image();
+	srcImage.onload = function() {
+	    drawSrcImageAndResize(srcImage, srcCanvas, dstCanvas);
 	}
-        var img = new Image();
-        img.onload = function() {
-	    var width = img.width, height = img.height;
-	    srcCanvas.width  = width;
-	    srcCanvas.height = height;
-	    srcCtx.drawImage(img, 0, 0, width, height);
-	    resizeFunc(srcCanvas, dstCanvas, valueTable);
-        }
-        img.src = dataURL;
-	valueTable["img"] = img;
+	srcImage.src = dataURL;
     }, "DataURL");
+    bindFunction({"maxWidthHeightRange":"maxWidthHeightText",
+		  "filterType":null,
+		  "scaleRange":"scaleText",
+		  "cubicBRange":"cubicBText",
+		  "cubicCRange":"cubicCText",
+		  "lobeRange":"lobeText"},
+		 function() {
+		     drawSrcImageAndResize(srcImage, srcCanvas, dstCanvas);
+		 } );
 }
 
-function makeFilterKernel(valueTable, srcWidth, srcHeight, dstWidth, dstHeight) {
+function drawSrcImageAndResize(srcImage, srcCanvas, dstCancas) {
+    var maxWidthHeight = parseFloat(document.getElementById("maxWidthHeightRange").value);
+    drawSrcImage(srcImage, srcCanvas, maxWidthHeight);
+    var params = {
+	filterType: document.getElementById("filterType").value,
+	scale: parseFloat(document.getElementById("scaleRange").value),
+	cubicB:parseFloat(document.getElementById("cubicBRange").value),
+	cubicC:parseFloat(document.getElementById("cubicCRange").value),
+	lobe:  parseFloat(document.getElementById("lobeRange").value)
+    };
+    drawGraph(params)
+    drawResize(srcCanvas, dstCanvas, params);
+}
+
+function makeFilterKernel(params, srcWidth, srcHeight, dstWidth, dstHeight) {
     if (srcWidth < dstWidth) {
 	var [width1, width2] = [srcWidth, dstWidth];
     } else {
@@ -75,7 +59,7 @@ function makeFilterKernel(valueTable, srcWidth, srcHeight, dstWidth, dstHeight) 
     var kernelWidth  = Math.ceil(2 * kernelWidthScale);
     var kernelHeight = Math.ceil(height2 / height1);
     var kernelScale = 1;
-    var filterType = valueTable["filterType"];
+    var filterType = params["filterType"];
     switch (filterType) {
     case "NN":
 	return null; // 存在しない
@@ -86,7 +70,7 @@ function makeFilterKernel(valueTable, srcWidth, srcHeight, dstWidth, dstHeight) 
 	kernelScale = 2;
 	break;
     case "Lanczos":
-	var lobe = valueTable["lobe"];
+	var lobe = params["lobe"];
 	kernelScale = lobe;
     }
     var kernel = new Array(kernelWidth * kernelHeight * kernelScale * kernelScale);
@@ -95,8 +79,8 @@ function makeFilterKernel(valueTable, srcWidth, srcHeight, dstWidth, dstHeight) 
 }
 
 
-function resizeFunc(srcCanvas, dstCanvas, valueTable) {
-    var scale = parseFloat(valueTable["logScale"]);
+function drawResize(srcCanvas, dstCanvas, params) {
+    var scale = params.scale;
     var srcWidth  = srcCanvas.width;
     var srcHeight = srcCanvas.height;
     var dstWidth  = Math.round(scale * srcWidth);
@@ -108,8 +92,8 @@ function resizeFunc(srcCanvas, dstCanvas, valueTable) {
     var srcImageData = srcCtx.getImageData(0, 0, srcWidth, srcHeight);
     var dstImageData = dstCtx.createImageData(dstWidth, dstHeight);
     var dstData = dstImageData.data;
-    var filterType = valueTable["filterType"];
-    var filterKernel = makeFilterKernel(valueTable, srcWidth, srcHeight, dstWidth, dstHeight);
+    var filterType = params.filterType;
+    var filterKernel = makeFilterKernel(params, srcWidth, srcHeight, dstWidth, dstHeight);
     for (var dstY = 0 ; dstY < dstHeight; dstY++) {
 	for (var dstX = 0 ; dstX < dstWidth; dstX++) {
 	    var dstOffset = 4 * (dstX + dstY * dstWidth);
@@ -193,4 +177,59 @@ function resizePixel(srcX, srcY, srcImageData, filterType, filterKernel) {
 	break;
     }
     return rgba;
+}
+
+function cubicBCcoefficient(b, c) {
+    var p = 12 - 9 * b - 6 * c;
+    var q = -18 + 12 * b + 6 * c;
+    var r = 0;
+    var s = 6 - 2 * b;
+    var t = -b - 6 * c;
+    var u = 6 * b + 30 * c;
+    var v = -12 * b - 48 * c;
+    var w = 8 * b + 24 * c;
+    return [p, q, r, s, t, u, v, w];
+}
+function cubicBC(x, coeff) {
+    var [p, q, r, s, t, u, v, w] = coeff;
+    var y = 0;
+    var ax = Math.abs(x);
+    if (ax < 1) {
+	y = (1/6) * (p*(ax*ax*ax) + q*(ax*ax) + r*(ax) + s);
+    } else if (ax < 2) {
+	y = (1/6) * (t*(ax*ax*ax) + u*(ax*ax) + v*(ax) + w);
+    }
+    return y;
+}
+
+function sinc(x) {
+    var pi_x = Math.PI * x;
+    return Math.sin(pi_x) / pi_x;
+}
+
+function sincFast(x) {
+    var xx = x * x;
+    // quantim depth 8
+    var c0 = 0.173610016489197553621906385078711564924e-2;
+    var c1 = -0.384186115075660162081071290162149315834e-3;
+    var c2 = 0.393684603287860108352720146121813443561e-4;
+    var c3 = -0.248947210682259168029030370205389323899e-5;
+    var c4 = 0.107791837839662283066379987646635416692e-6;
+    var c5 = -0.324874073895735800961260474028013982211e-8;
+    var c6 = 0.628155216606695311524920882748052490116e-10;
+    var c7 = -0.586110644039348333520104379959307242711e-12;
+    var p =
+	c0+xx*(c1+xx*(c2+xx*(c3+xx*(c4+xx*(c5+xx*(c6+xx*c7))))));
+    return (xx-1.0)*(xx-4.0)*(xx-9.0)*(xx-16.0)*p;
+}
+
+function lanczos(x, lobe) {
+    if (x === 0) {
+	return 0;
+    }
+    if (Math.abs(x) < lobe) {
+	//return sinc(x) * sinc(x/lobe);
+	return sincFast(x) * sincFast(x/lobe);
+    }
+    return 0;
 }
