f15618d44e19e911c25127445d7ba336f5875dfb
Yoshihiro Yamazaki <yoya@awm.jp>
2017-06-10 21:02:26 +0900

    add color matching function graph

--- cie.js
+++ cie.js
@@ -1,6 +1,6 @@
 "use strict";
 /*
- * 2017/04/05- (c) yoya@awm.jp
+ * 2017/06/10- (c) yoya@awm.jp
  */
 
 document.addEventListener("DOMContentLoaded", function(event) {
@@ -32,7 +32,12 @@ function main() {
 	xhr.onreadystatechange = function() {
 	    if (xhr.readyState === 4) {
 		cieArr = JSON.parse(xhr.responseText);
+		cieArr = cieArr.filter(function(e) {
+		    var lw =  e[0]; // length of wave
+		    return (370 < lw) && (lw < 720);
+		});
 		drawDiagram(dstCanvas, cieArr, hist);
+		drawGraph(graphCanvas, cieArr, hist);
 	    }
 	};
 	xhr.open("GET", file, true); // async:true
@@ -77,6 +82,16 @@ function drawDiagram(dstCanvas, cieArr, hist) {
 	drawDiagramPoint(dstCanvas, hist, colorspace);
     }
 }
+
+function drawGraph(canvas, cieArr, hist) {
+    var colorspace = document.getElementById("colorspaceSelect").value;
+    canvas.width  = canvas.width ; // clear
+    drawGraphBase(canvas, cieArr, colorspace);
+    if (hist !== null) {
+	drawGraphPoint(dstCanvas, hist);
+    }
+}
+
 
 function graphTrans(xy, width, height) {
     var [x, y] = xy;
@@ -175,4 +190,69 @@ function drawDiagramPoint(dstCanvas, hist, colorspace) {
 	}
 	ctx.fillRect(gx, gy, 0.5, 0.5);
     }
+}
+
+function drawGraphBase(canvas, cieArr) {
+    // console.debug("drawGraphBase", canvas, cieArr);
+    canvas.style.backgroundColor = "gray";
+    var width = canvas.width;
+    var height = canvas.height;
+    var ctx = canvas.getContext("2d");
+    var lxArr = [], lyArr = [], lzArr = [];
+    var xyRatioTable = [];
+    var maxValue = 0;
+    var i = 0;
+    var arrLen = cieArr.length;
+    var grad = ctx.createLinearGradient(0, 0, width, 0);
+    for (var data of cieArr) {
+	var [wl, lx, ly, lz] = data;
+	lxArr.push(lx);
+	lyArr.push(ly);
+	lzArr.push(lz);
+	if (wl < 445) {
+	    // wl: 440
+	    var [x, y, z] = [0.348280000000,0.023000000000,1.747060000000];
+	    var a = lz / 1.747060000000;
+	} else if (wl <= 600) {
+	    var [x, y, z] = [lx, ly, lz];
+	    var a = 1.0;
+	} else {
+	    // wl: 605
+	    var [x, y, z] = [1.045600000000,0.566800000000,0.000600000000];
+	    var a = lx / 1.045600000000;
+	}
+	var lrgb = XYZ2RGB([x, y, z]);
+	var [r, g, b] = linearRGB2sRGB(lrgb);
+	var color = "rgba("+r+","+g+","+b+","+a+")";
+	grad.addColorStop(i / arrLen, color);
+	i++;
+    }
+    ctx.fillStyle = grad;
+    ctx.rect(0, 0, width, height);
+    ctx.fill();
+    var lxMax = Math.max.apply(null, lxArr);
+    var lyMax = Math.max.apply(null, lyArr);
+    var lzMax = Math.max.apply(null, lzArr);
+    var lMax = Math.max.apply(null, [lxMax, lyMax, lzMax]);
+    var graphLines = [["#F88", lxArr], ["#AFA", lyArr], ["#88F", lzArr]];
+    // ctx.globalCompositeOperation = "lighter";
+    for (var i in graphLines) {
+	var [color, arr] = graphLines[i];
+	ctx.strokeStyle = color;
+	ctx.lineWidth = 3;
+	ctx.beginPath();
+	ctx.moveTo(0, height);
+	for (var j = 1, n = arr.length ; j < n ; j++) {
+	    var x = width * j / n;
+	    ctx.lineTo(x, height * (1 - arr[j] / lMax));
+	}
+	ctx.stroke();
+    }
+}
+
+function drawGraphPoint(canvas, hist) {
+    var width = canvas.width;
+    var height = canvas.height;
+    var ctx = canvas.getContext("2d");
+    ctx.fillStyle = "black";
 }
