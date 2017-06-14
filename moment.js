d5adffff4f3acb370224f4f8d58d68e6c0a3f50e
Yoshihiro Yamazaki <yoya@awm.jp>
2017-06-15 01:31:08 +0900

    add Axis from 2nd moment.

--- moment.js
+++ moment.js
@@ -38,8 +38,9 @@ function calcMoment(canvas, reverse) {
     var ctx = canvas.getContext("2d");
     var imageData = ctx.getImageData(0, 0, width, height);
     var M00 = 0, M10 = 0, M01 = 0;
-    for (var y = 0 ; y < height ; y++) {
-	for (var x = 0 ; x < width ; x++) {
+    var M11 = 0, M20 = 0, M02 = 0;
+    for (var y = 0, y_y = -height/2; y < height ; y++, y_y++) {
+	for (var x = 0, x_x = -width/2 ; x < width ; x++, x_x++) {
 	    var [v] = getRGBA(imageData, x, y);
 	    if (reverse) {
 		v = 255 - v;
@@ -47,9 +48,12 @@ function calcMoment(canvas, reverse) {
 	    M00 += v;
 	    M10 += x * v;
 	    M01 += y * v;
+	    M11 += x_x * y_y * v;
+	    M20 += x_x * x_x * v;
+	    M02 += y_y * y_y * v;
 	}
     }
-    return [ M00, M10, M01 ];
+    return [ M00, M10, M01, M11, M20, M02 ];
 }
 
 function drawMoment(canvas, reverse) {
@@ -60,18 +64,41 @@ function drawMoment(canvas, reverse) {
     var M01Text = document.getElementById("M01Text");
     var width = canvas.width, height = canvas.height;
     //
-    var [ M00, M10, M01 ] = calcMoment(canvas, reverse);
-    // console.debug([ M00, M10, M01 ]);
+    var [ M00, M10, M01, M11, M20, M02 ] = calcMoment(canvas, reverse);
+    console.debug([ M00, M10, M01, M11, M20, M02 ]);
     M00Text.value = M00;
     M10Text.value = M10;
     M01Text.value = M01;
     var [cgX, cgY] = [M10 / M00, M01 / M00];
+    var sqrt_2 = Math.sqrt(2);
+    var M20_M01 = M20 - M01;
+    var tmp = Math.sqrt(M20_M01 * M20_M01 + 4 * M11*M11)
+    var majorAxis = sqrt_2 * Math.sqrt(M20 + M02 + tmp);
+    var minorAxis = sqrt_2 * Math.sqrt(M20 + M02 - tmp);
+    var theta = 1/2 * Math.atan2(2* M11 , (M20 - M02));
     // console.debug([ cgX, cgY ]);
+    console.debug([ majorAxis, minorAxis, theta ]);
+    // Gravity Center
     ctx.beginPath();
-    ctx.fillStyle = "rgba(255, 0, 0, 0.5)";
-    ctx.strokeStyle = "orange";
+    ctx.fillStyle = "rgba(255, 0, 0, 0.3)";
+    ctx.strokeStyle = "rgb(255,0,255)";
+    ctx.lineWidth = 2;
     ctx.arc(cgX, cgY, 10, 0, 2*Math.PI);
     ctx.closePath();
     ctx.fill();
+    ctx.stroke();
+    // Axis
+    var size = (width + height) / 3;
+    ctx.strokeStyle = "rgba(0,255,255, 0.5)";
+    ctx.lineWidth = 3;
+    ctx.beginPath();
+    ctx.moveTo(cgX - Math.cos(theta)*size, cgY - Math.sin(theta)*size);
+    ctx.lineTo(cgX + Math.cos(theta)*size, cgY + Math.sin(theta)*size);
+    ctx.stroke();
+    ctx.strokeStyle = "rgba(255,255,0, 0.5)";
+    ctx.beginPath();
+    var theta2 = theta + Math.PI /2;
+    ctx.moveTo(cgX - Math.cos(theta2)*size, cgY - Math.sin(theta2)*size);
+    ctx.lineTo(cgX + Math.cos(theta2)*size, cgY + Math.sin(theta2)*size);
     ctx.stroke();
 }
