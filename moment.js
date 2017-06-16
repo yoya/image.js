e64f220330e8560def1ec03b3d8afe72bf8c9b22
Yoshihiro Yamazaki <yoya@awm.jp>
2017-06-16 23:43:35 +0900

    fix calculation for M11, M20, M02.

--- moment.js
+++ moment.js
@@ -38,9 +38,25 @@ function calcMoment(canvas, reverse) {
     var ctx = canvas.getContext("2d");
     var imageData = ctx.getImageData(0, 0, width, height);
     var M00 = 0, M10 = 0, M01 = 0;
-    var M11 = 0, M20 = 0, M02 = 0;
     for (var y = 0, y_y = -height/2; y < height ; y++, y_y++) {
 	for (var x = 0, x_x = -width/2 ; x < width ; x++, x_x++) {
+	    var [v, g, b, a] = getRGBA(imageData, x, y);
+	    if (reverse) {
+		v = 255 - v;
+	    }
+	    v *= a/255;
+	    M00 += v;
+	    M10 += x * v;
+	    M01 += y * v;
+	    M11 += x_x * y_y * v;
+	    M20 += x_x * x_x * v;
+	    M02 += y_y * y_y * v;
+	}
+    }
+    var gx = M10 / M00, gy = M01 / M00;
+    var M11 = 0, M20 = 0, M02 = 0;
+    for (var y = 0, y_y = -gy; y < height ; y++, y_y++) {
+	for (var x = 0, x_x = -gx ; x < width ; x++, x_x++) {
 	    var [v, g, b, a] = getRGBA(imageData, x, y);
 	    if (reverse) {
 		v = 255 - v;
