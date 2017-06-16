524d3692255d06ce1b230b53bf58e70df2a7a1e0
Yoshihiro Yamazaki <yoya@awm.jp>
2017-06-17 01:33:26 +0900

    add fisheyeCheckbox

--- twittericon.js
+++ twittericon.js
@@ -19,45 +19,77 @@ function main() {
 	}
 	srcImage.src = dataURL;
     }, "DataURL");
-    bindFunction({"maxWidthHeightRange":"maxWidthHeightText"},
+    bindFunction({"maxWidthHeightRange":"maxWidthHeightText",
+		  "fisheyeCheckbox":null},
 		 function() {
 		     drawSrcImageAndCopy(srcImage, srcCanvas, dstCanvas);
 		 } );
 }
 function drawSrcImageAndCopy(srcImage, srcCanvas, dstCancas) {
     var maxWidthHeight = parseFloat(document.getElementById("maxWidthHeightRange").value);
+    var fisheye = document.getElementById("fisheyeCheckbox").checked;
     drawSrcImage(srcImage, srcCanvas, maxWidthHeight);
-    drawCopy(srcCanvas, dstCanvas);
+    drawCopy(srcCanvas, dstCanvas, fisheye);
 }
 
-
-function drawCopy(srcCanvas, dstCanvas) {
+function drawCopy(srcCanvas, dstCanvas, fisheye) {
     // console.debug("drawCopy");
     var srcCtx = srcCanvas.getContext("2d");
     var dstCtx = dstCanvas.getContext("2d");
     var srcWidth = srcCanvas.width, srcHeight = srcCanvas.height;
-    var radius = Math.sqrt(srcWidth*srcWidth + srcHeight*srcHeight) / 2;
-    
-    var dstWidth  = Math.ceil(radius*2);
-    var dstHeight = dstWidth;
     dstCanvas.style.backgroundColor = "white";
-    dstCanvas.width  = dstWidth;
-    dstCanvas.height = dstHeight;
-    /*
-    var dstImageData = dstCtx.createImageData(dstWidth, dstHeight);
-    for (var dstY = 0 ; dstY < dstHeight; dstY++) {
-        for (var dstX = 0 ; dstX < dstWidth; dstX++) {
-	    setRGBA(dstImageData, dstX, dstY, [255, 255, 255, 0]);
+    if (fisheye) {
+	var dstWidth  = srcWidth;
+	var dstHeight = srcHeight;
+	dstCanvas.width  = dstWidth;
+	dstCanvas.height = dstHeight;
+	var srcImageData = srcCtx.getImageData(0, 0, srcWidth, srcHeight);
+	var dstImageData = dstCtx.createImageData(dstWidth, dstHeight);
+	// http://q.hatena.ne.jp/1347662738
+	for (var dstY = 0 ; dstY < dstHeight; dstY++) {
+            for (var dstX = 0 ; dstX < dstWidth; dstX++) {
+		var kk= srcWidth * 0.5;
+		var ll= srcHeight * 0.5;
+		var sr = Math.min(kk, ll);
+		var dx = dstX - kk;
+		var dy = dstY - ll;
+		var rr = Math.hypot(dx, dy);
+		if (rr < sr) {
+		    var pr = 1 - 2*Math.acos(rr/sr)/Math.PI;
+		    var px = (rr==0.0) ? 0.0 : (pr*dx*sr/rr);
+		    var py = (rr==0.0) ? 0.0 : (pr*dy*sr/rr);
+		    var srcX = Math.round(px + kk);
+		    var srcY = Math.round(py + ll);
+		    var rgba = getRGBA(srcImageData, srcX, srcY);
+		    setRGBA(dstImageData, dstX, dstY, rgba);
+		} else {
+		    // nothing to do
+		}
+	    }
 	}
+	dstCtx.putImageData(dstImageData, 0, 0);
+    } else {
+	var radius = Math.sqrt(srcWidth*srcWidth + srcHeight*srcHeight) / 2;
+	var dstWidth  = Math.ceil(radius*2);
+	var dstHeight = dstWidth;
+	dstCanvas.width  = dstWidth;
+	dstCanvas.height = dstHeight;
+	/*
+	  var dstImageData = dstCtx.createImageData(dstWidth, dstHeight);
+	  for (var dstY = 0 ; dstY < dstHeight; dstY++) {
+          for (var dstX = 0 ; dstX < dstWidth; dstX++) {
+	  setRGBA(dstImageData, dstX, dstY, [255, 255, 255, 0]);
+	  }
+	  }
+	  setRGBA(dstImageData, dstWidth - 1, dstHeight - 1, [255, 255, 255, 1]);
+	  dstCtx.putImageData(dstImageData, 0, 0);
+	*/
+	var x1 = radius - srcWidth/2;
+	var y1 = radius - srcHeight/2;
+	var x2 = dstWidth  - 2 * x1;
+	var y2 = dstHeight - 2 * y1;
+	console.log(Math.ceil(x1), Math.ceil(y1),Math.floor(x2),Math.floor(y2) );
+	dstCtx.drawImage(srcCanvas, 0, 0, srcCanvas.width, srcCanvas.height,
+			 x1, y1, x2, y2);
     }
-    setRGBA(dstImageData, dstWidth - 1, dstHeight - 1, [255, 255, 255, 1]);
-    dstCtx.putImageData(dstImageData, 0, 0);
-    */
-    var x1 = radius - srcWidth/2;
-    var y1 = radius - srcHeight/2;
-    var x2 = dstWidth  - 2 * x1;
-    var y2 = dstHeight - 2 * y1;
-    console.log(Math.ceil(x1), Math.ceil(y1),Math.floor(x2),Math.floor(y2) );
-    dstCtx.drawImage(srcCanvas, 0, 0, srcCanvas.width, srcCanvas.height,
-		     x1, y1, x2, y2);
 }
