703270bb69e070695495d1f99790f8ffa44d4929
Yoshihiro Yamazaki <yoya@awm.jp>
2017-06-17 14:49:24 +0900

    add Doom Projection

--- twittericon.js
+++ twittericon.js
@@ -21,78 +21,35 @@ function main() {
     }, "DataURL");
     bindFunction({"maxWidthHeightRange":"maxWidthHeightText",
 		  "outfillSelect":null,
-		  "fisheyeCheckbox":null,
+		  "projSelect":null,
 		  "srcProjXRange":"srcProjXText",
 		  "srcProjYRange":"srcProjYText",
 		  "srcProjRRange":"srcProjRText"},
-		 function() {
+		 function(e) {
+		     console.debug(e);
 		     drawSrcImageAndCopy(srcImage, srcCanvas, dstCanvas);
 		 } );
 }
 function drawSrcImageAndCopy(srcImage, srcCanvas, dstCancas) {
     var maxWidthHeight = parseFloat(document.getElementById("maxWidthHeightRange").value);
     var outfill = document.getElementById("outfillSelect").value;
-    var fisheye = document.getElementById("fisheyeCheckbox").checked;
+    var proj = document.getElementById("projSelect").value;
     var srcProjX = parseFloat(document.getElementById("srcProjXRange").value);
     var srcProjY = parseFloat(document.getElementById("srcProjYRange").value);
     var srcProjR = parseFloat(document.getElementById("srcProjRRange").value);
     drawSrcImage(srcImage, srcCanvas, maxWidthHeight);
     drawCopy(srcCanvas, dstCanvas, outfill,
-	     fisheye, srcProjX, srcProjY, srcProjR);
+	     proj, srcProjX, srcProjY, srcProjR);
 }
 
 function drawCopy(srcCanvas, dstCanvas, outfill,
-		  fisheye, srcProjX, srcProjY, srcProjR) {
+		  proj, srcProjX, srcProjY, srcProjR) {
     // console.debug("drawCopy");
     var srcCtx = srcCanvas.getContext("2d");
     var dstCtx = dstCanvas.getContext("2d");
     var srcWidth = srcCanvas.width, srcHeight = srcCanvas.height;
     dstCanvas.style.backgroundColor = "white";
-    if (fisheye) {
-	var dstWidth  = Math.min(srcWidth, srcHeight);
-	var dstHeight = dstWidth;
-	dstCanvas.width  = dstWidth;
-	dstCanvas.height = dstHeight;
-	var srcImageData = srcCtx.getImageData(0, 0, srcWidth, srcHeight);
-	var dstImageData = dstCtx.createImageData(dstWidth, dstHeight);
-	// http://q.hatena.ne.jp/1347662738
-	for (var dstY = 0 ; dstY < dstHeight; dstY++) {
-            for (var dstX = 0 ; dstX < dstWidth; dstX++) {
-		var kk= dstWidth * 0.5;
-		var ll= dstHeight * 0.5;
-		var sr = Math.min(kk, ll);
-		var dx = dstX - kk;
-		var dy = dstY - ll;
-		var rr = Math.hypot(dx, dy);
-		if (rr < sr) {
-		    var pr = 1 - 2*Math.acos(rr/sr)/Math.PI;
-		    pr *= srcProjR;
-		    var px = (rr==0.0) ? 0.0 : (pr*dx*sr/rr);
-		    var py = (rr==0.0) ? 0.0 : (pr*dy*sr/rr);
-		    var srcX = Math.round(px + srcWidth*srcProjX);
-		    var srcY = Math.round(py + srcHeight*srcProjY);
-		    var rgba = getRGBA(srcImageData, srcX, srcY, outfill);
-		    setRGBA(dstImageData, dstX, dstY, rgba);
-		} else {
-		    if (outfill === "white") {
-			var rgba = [255,255,255,255];
-		    } else if (outfill === "black") {
-			var rgba = [0, 0, 0, 255];
-		    } else {
-			var pr = 1 - 2*Math.acos(1)/Math.PI;
-			pr *= srcProjR;
-			var px = (rr==0.0) ? 0.0 : (pr*dx*sr/rr);
-			var py = (rr==0.0) ? 0.0 : (pr*dy*sr/rr);
-			var srcX = Math.round(px + srcWidth*srcProjX);
-			var srcY = Math.round(py + srcHeight*srcProjY);
-			var rgba = getRGBA(srcImageData, srcX, srcY, outfill);
-		    }
-		    setRGBA(dstImageData, dstX, dstY, rgba);
-		}
-	    }
-	}
-	dstCtx.putImageData(dstImageData, 0, 0);
-    } else {
+    if (proj === "normal") {
 	var radius = Math.sqrt(srcWidth*srcWidth + srcHeight*srcHeight) / 2;
 	var dstWidth  = Math.ceil(radius*2);
 	var dstHeight = dstWidth;
@@ -110,6 +67,58 @@ function drawCopy(srcCanvas, dstCanvas, outfill,
 		var srcY = dstY - y1;
 		var rgba = getRGBA(srcImageData, srcX, srcY, outfill);
 		setRGBA(dstImageData, dstX, dstY, rgba);
+	    }
+	}
+	dstCtx.putImageData(dstImageData, 0, 0);
+    } else {
+	var dstWidth  = Math.min(srcWidth, srcHeight);
+	var dstHeight = dstWidth;
+	dstCanvas.width  = dstWidth;
+	dstCanvas.height = dstHeight;
+	var srcImageData = srcCtx.getImageData(0, 0, srcWidth, srcHeight);
+	var dstImageData = dstCtx.createImageData(dstWidth, dstHeight);
+	// http://q.hatena.ne.jp/1347662738
+	for (var dstY = 0 ; dstY < dstHeight; dstY++) {
+            for (var dstX = 0 ; dstX < dstWidth; dstX++) {
+		var kk= dstWidth * 0.5;
+		var ll= dstHeight * 0.5;
+		var sr = Math.min(kk, ll);
+		var dx = dstX - kk;
+		var dy = dstY - ll;
+		var rr = Math.hypot(dx, dy);
+		if (rr < sr) {
+		    if (proj === "fisheye") {
+			var pr = 1 - 2*Math.acos(rr/sr)/Math.PI;
+		    } else { // doom
+			var pr = 2 - 4*(0.5-Math.atan2(rr,sr)/Math.PI);
+		    }
+		    pr *= srcProjR;
+		    var px = (rr==0.0) ? 0.0 : (pr*dx*sr/rr);
+		    var py = (rr==0.0) ? 0.0 : (pr*dy*sr/rr);
+		    var srcX = Math.round(px + srcWidth*srcProjX);
+		    var srcY = Math.round(py + srcHeight*srcProjY);
+		    var rgba = getRGBA(srcImageData, srcX, srcY, outfill);
+		    setRGBA(dstImageData, dstX, dstY, rgba);
+		} else {
+		    if (outfill === "white") {
+			var rgba = [255,255,255,255];
+		    } else if (outfill === "black") {
+			var rgba = [0, 0, 0, 255];
+		    } else {
+			if (proj === "fisheye") {
+			    var pr = 1 - 2*Math.acos(1)/Math.PI;
+			} else { // doom
+			    var pr = 2 - 4*(0.5-Math.atan2(1, 0)/Math.PI);
+			}
+			pr *= srcProjR;
+			var px = (rr==0.0) ? 0.0 : (pr*dx*sr/rr);
+			var py = (rr==0.0) ? 0.0 : (pr*dy*sr/rr);
+			var srcX = Math.round(px + srcWidth*srcProjX);
+			var srcY = Math.round(py + srcHeight*srcProjY);
+			var rgba = getRGBA(srcImageData, srcX, srcY, outfill);
+		    }
+		    setRGBA(dstImageData, dstX, dstY, rgba);
+		}
 	    }
 	}
 	dstCtx.putImageData(dstImageData, 0, 0);
