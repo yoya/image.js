a40f538a72df53a1636144963a9ec1b402361d9b
Yoshihiro Yamazaki <yoya@awm.jp>
2017-07-01 23:48:28 +0900

    rescale interface, canvas to imagedata

--- rescale.js
+++ rescale.js
@@ -1,8 +1,7 @@
 "use strict";
-// require func.js
-// require bind.js
-// require graph.js
-// require image.js
+/*
+* 2016/11/13- yoya@awm.jp . All Rights Reserved.
+*/
 
 main();
 
@@ -32,6 +31,8 @@ function main() {
 function drawSrcImageAndRescale(srcImage, srcCanvas, dstCancas) {
     var maxWidthHeight = parseFloat(document.getElementById("maxWidthHeightRange").value);
     drawSrcImage(srcImage, srcCanvas, maxWidthHeight);
+    var srcCtx = srcCanvas.getContext("2d");
+    var dstCtx = dstCanvas.getContext("2d");
     var params = {
 	filterType: document.getElementById("filterType").value,
 	scale: parseFloat(document.getElementById("scaleRange").value),
@@ -40,87 +41,41 @@ function drawSrcImageAndRescale(srcImage, srcCanvas, dstCancas) {
 	lobe:  parseFloat(document.getElementById("lobeRange").value)
     };
     drawGraph(params)
-    drawRescale(srcCanvas, dstCanvas, params);
-}
-
-function makeFilterKernel(params, srcWidth, srcHeight, dstWidth, dstHeight) {
-    if (srcWidth < dstWidth) {
-	var [width1, width2] = [srcWidth, dstWidth];
-    } else {
-	var [width1, width2] = [dstWidth, srcWidth];
-    }
-    if (srcHeight < dstHeight) {
-	var [height1, height2] = [srcHeight, dstHeight];
-    } else {
-	var [height1, height2] = [dstHeight, srcHeight];
-    }
-    var kernelWidthScale  = width2 / width1
-    var kernelHeightScale = height2 / height1
-    var kernelWidth  = Math.ceil(2 * kernelWidthScale);
-    var kernelHeight = Math.ceil(height2 / height1);
-    var kernelScale = 1;
-    var filterType = params["filterType"];
-    switch (filterType) {
-    case "NN":
-	return null; // 存在しない
-    case "BiLinear":
-	kernelScale = 1;
-	break;
-    case "BiCubic":
-	kernelScale = 2;
-	break;
-    case "Lanczos":
-	var lobe = params["lobe"];
-	kernelScale = lobe;
-    }
-    var kernel = new Array(kernelWidth * kernelHeight * kernelScale * kernelScale);
-    
-    return kernel;
-}
-
-
-function drawRescale(srcCanvas, dstCanvas, params) {
-    var scale = params.scale;
     var srcWidth  = srcCanvas.width;
     var srcHeight = srcCanvas.height;
+    var srcImageData = srcCtx.getImageData(0, 0, srcWidth, srcHeight);
+    var dstImageData = drawRescale(srcImageData, params);
+    var dstWidth  = dstImageData.width;
+    var dstHeight = dstImageData.height;
+    dstCanvas.width = dstWidth;
+    dstCanvas.height = dstHeight;
+    dstCtx.putImageData(dstImageData, 0, 0);
+}
+
+function drawRescale(srcImageData, params) {
+    // console.debug("drawRescale", srcImageData, params);
+    var scale = params.scale;
+    var filterType = params.filterType;
+    var srcWidth  = srcImageData.width;
+    var srcHeight = srcImageData.height;
     var dstWidth  = Math.round(scale * srcWidth);
     var dstHeight = Math.round(scale * srcHeight);
-    dstCanvas.width  = dstWidth;
-    dstCanvas.height = dstHeight;
-    var srcCtx = srcCanvas.getContext("2d");
-    var dstCtx = dstCanvas.getContext("2d");
-    var srcImageData = srcCtx.getImageData(0, 0, srcWidth, srcHeight);
-    var dstImageData = dstCtx.createImageData(dstWidth, dstHeight);
+    var dstImageData = new ImageData(dstWidth, dstHeight);
     var dstData = dstImageData.data;
-    var filterType = params.filterType;
-    var filterKernel = makeFilterKernel(params, srcWidth, srcHeight, dstWidth, dstHeight);
     for (var dstY = 0 ; dstY < dstHeight; dstY++) {
 	for (var dstX = 0 ; dstX < dstWidth; dstX++) {
 	    var dstOffset = 4 * (dstX + dstY * dstWidth);
 	    var srcX = dstX / scale;
 	    var srcY = dstY / scale;
-	    var [r,g,b,a] = rescalePixel(srcX, srcY, srcImageData, filterType, filterKernel);
-	    dstData[dstOffset]   = r;
-	    dstData[dstOffset+1] = g;
-	    dstData[dstOffset+2] = b;
-	    dstData[dstOffset+3] = a;
+	    var [r,g,b,a] = rescalePixel(srcX, srcY, srcImageData, filterType);
+	    setRGBA(dstImageData, dstX, dstY, [r,g,b,a]);
 	}
     }
-    dstCtx.putImageData(dstImageData, 0, 0);
+    return dstImageData;
 }
 
-function getPixel(imageData, x, y) {
-    var data = imageData.data;
-    var width  = imageData.width;
-    var height = imageData.height;
-    x = (x < 0)? 0 : ((x < width )?x:width -1);
-    y = (y < 0)? 0 : ((y < height)?y:height-1);
-    var offset = 4 * (x + y * width);
-    return data.slice(offset, offset+4); // [r,g,b,a]
-}
-
-function getPixel_NN(imageData, x, y) {
-    return getPixel(imageData, Math.floor(x), Math.floor(y));
+function getRGBA_NN(imageData, x, y) {
+    return getRGBA(imageData, Math.floor(x), Math.floor(y));
 }
 
 function linearRatio([a, b], p) {
@@ -131,7 +86,7 @@ function linearRatio([a, b], p) {
     return [aRatio, 1 - aRatio];
 }
 
-function getPixel_BiLinear(imageData, x, y) {
+function getRGBA_BiLinear(imageData, x, y) {
     var data = imageData.data;
     var width  = imageData.width;
     var height = imageData.height;
@@ -154,24 +109,24 @@ function getPixel_BiLinear(imageData, x, y) {
     var r12 = rx1 * ry2;
     var r21 = rx2 * ry1;
     var r22 = rx2 * ry2;
-    var rgba11 = getPixel(imageData, x1, y1);
-    var rgba12 = getPixel(imageData, x1, y2);
-    var rgba21 = getPixel(imageData, x2, y1);
-    var rgba22 = getPixel(imageData, x2, y2);
+    var rgba11 = getRGBA(imageData, x1, y1);
+    var rgba12 = getRGBA(imageData, x1, y2);
+    var rgba21 = getRGBA(imageData, x2, y1);
+    var rgba22 = getRGBA(imageData, x2, y2);
     for (var i = 0 ; i < 4 ; i++) {
 	rgba[i] = r11 * rgba11[i] +  r12 * rgba12[i] + r21 * rgba21[i] + r22 * rgba22[i];
     }
     return rgba;
 }
 
-function rescalePixel(srcX, srcY, srcImageData, filterType, filterKernel) {
+function rescalePixel(srcX, srcY, srcImageData, filterType) {
     var rgba = null;
     switch (filterType) {
     case "NN":
-	rgba = getPixel_NN(srcImageData, srcX, srcY);
+	rgba = getRGBA_NN(srcImageData, srcX, srcY);
 	break;
     case "BiLinear":
-	rgba = getPixel_BiLinear(srcImageData, srcX, srcY);
+	rgba = getRGBA_BiLinear(srcImageData, srcX, srcY);
 	break;
     default:
 	rgba = [255, 0, 0, 255];
