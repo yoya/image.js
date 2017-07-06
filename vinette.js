e6ca83c9f8d4da4ff074280c7cc88384d4700f5b
Yoshihiro Yamazaki <yoya@awm.jp>
2017-07-07 00:42:27 +0900

    linearGamma for Vinette

--- vinette.js
+++ vinette.js
@@ -19,19 +19,21 @@ function main() {
 	}
 	srcImage.src = dataURL;
     }, "DataURL");
-    bindFunction({"maxWidthHeightRange":"maxWidthHeightText"},
+    bindFunction({"maxWidthHeightRange":"maxWidthHeightText",
+		  "linearGammaCheckbox":null},
 		 function() {
 		     drawSrcImageAndVinette(srcImage, srcCanvas, dstCanvas);
 		 } );
 }
 function drawSrcImageAndVinette(srcImage, srcCanvas, dstCancas) {
     var maxWidthHeight = parseFloat(document.getElementById("maxWidthHeightRange").value);
+    var linearGamma = document.getElementById("linearGammaCheckbox").checked;
     drawSrcImage(srcImage, srcCanvas, maxWidthHeight);
-    drawVinette(srcCanvas, dstCanvas);
+    drawVinette(srcCanvas, dstCanvas, linearGamma);
 }
 
 
-function drawVinette(srcCanvas, dstCanvas) {
+function drawVinette(srcCanvas, dstCanvas, linearGamma) {
     // console.debug("drawVinette");
     var srcCtx = srcCanvas.getContext("2d");
     var dstCtx = dstCanvas.getContext("2d");
@@ -48,10 +50,19 @@ function drawVinette(srcCanvas, dstCanvas) {
             var dy = (y - (height / 2)) / (longSide / 2);
             var r = Math.sqrt(dx*dx + dy*dy);
 	    var factor = Math.pow(Math.cos(r/2.0), 4);
-	    var [r, g, b, a] = getRGBA(srcImageData, x, y);
-	    r *= factor;
-	    g *= factor;
-	    b *= factor;
+	    if (linearGamma) {
+		var rgba = getRGBA(srcImageData, x, y);
+		var [lr, lg, lb, la] = sRGB2linearRGB(rgba);
+		lr *= factor;
+		lg *= factor;
+		lb *= factor;
+		[r, g, b, a] = linearRGB2sRGB([lr, lg, lb, la]);
+	    } else {
+		var [r, g, b, a] = getRGBA(srcImageData, x, y);
+		r *= factor;
+		g *= factor;
+		b *= factor;
+	    }
 	    setRGBA(dstImageData, x, y, [r, g, b, a]);
 	}
     }
