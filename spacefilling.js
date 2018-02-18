a8de950f63729b245f4580b68e4d9c59b0855b3e
Yoshihiro Yamazaki <yoya@awm.jp>
2018-02-18 22:18:00 +0900

    fix scaleNotenameTable

--- spacefilling.js
+++ spacefilling.js
@@ -301,7 +301,7 @@ function drawSpaceFilling(canvas, params) {
 
 var scaleNotenameTable = {
     'C':0, 'C+':1, 'D+':1,
-    'D':2, 'D+':3, 'D-':3,
+    'D':2, 'D+':3, 'E-':3,
     'E':4, 'F':5, 'F+':6, 'G-':6,
     'G':7, 'G+':8, 'A-':8,
     'A':9, 'A+':10, 'B':10, 'H':11,
