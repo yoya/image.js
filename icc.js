15e1dc59c093d92848e39ea8eb9df614c7d03892
Yoshihiro Yamazaki <yoya@awm.jp>
2018-02-20 00:29:57 +0900

    header output to HTML table.

--- icc.js
+++ icc.js
@@ -13,6 +13,8 @@ function main() {
     //var imageClassList = [IO_JPEG, IO_PNG, IO_GIF, IO_TIFF, IO_BMP];
     var imageClassList = [IO_JPEG, IO_PNG];
     dropFunction(document, function(buf) {
+	var consoleText = document.getElementById("consoleText");
+	consoleText.value = "----";
 	var arr = new Uint8Array(buf);
 	var io = null;
 	for (var imgClass of imageClassList) {
@@ -22,14 +24,43 @@ function main() {
 	}
 	if (! io) {
 	    console.warn("can't accept format");
+	    consoleText.value = "can't accept format";
 	    return ;
 	}
 	io.parse(arr);
         var iccdata = io.getICC();
 	io = null;
+	if (iccdata === null) {
+	    console.warn("can't find icc");
+	    consoleText.value = "can't find icc";
+	    return ;
+	}
 	var icc = new IO_ICC();
-	icc.parse(iccdata);
-	// return String.fromCharCode.apply(null, this);
+	if (icc.parse(iccdata) === false) {
+	    console.warn("icc parse error");
+	    consoleText.value = "icc parse error";
+	    return ;
+	}
+	var header = icc.getHeader();
+	var tagTable = icc.getTagTable();
+	console.log(header);
+	console.log(tagTable);
+	var iccHeaderTable = document.getElementById("iccHeaderTable");
+	for (name in header) {
+	    var value = header[name];
+	    var tr = document.createElement("tr");
+	    var th = document.createElement("th");
+	    var td = document.createElement("td");
+	    th.appendChild(document.createTextNode(name));
+	    if ((typeof(value) !== "string") && (typeof(value) !== "number")) {
+		value = Object.values(value).join(", ");
+	    }
+	    td.appendChild(document.createTextNode(value));
+	    tr.appendChild(th);
+	    tr.appendChild(td);
+	    iccHeaderTable.appendChild(tr);
+	}
+	var iccTagTable = document.getElementById("iccTagTable");
     }, "ArrayBuffer");
 }
 
