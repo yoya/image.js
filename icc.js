9c29ad5959e05227d3f29cb5aa576d1268d70795
Yoshihiro Yamazaki <yoya@awm.jp>
2018-02-22 00:25:43 +0900

    display CIE chromaticity diagram from ICC tristimulus.

--- icc.js
+++ icc.js
@@ -42,6 +42,19 @@ function main() {
     var containerNode = document.getElementById("ibv_container");
     //var imageClassList = [IO_JPEG, IO_PNG, IO_GIF, IO_TIFF, IO_BMP];
     var imageClassList = [IO_ICC, IO_JPEG, IO_PNG];
+    var params = {
+	'chromaticity':'ciexy',
+        'colorspace':'srgb',
+        'tristimulus':true,
+        'guide':true
+    };
+    var onCIEXYZdata = function(name, arr, isDefault) {
+	params[name] = arr;
+	if (isDefault) {
+	    params['cieArr'] = arr;
+	}
+    }
+    loadCIEXYZdata(onCIEXYZdata);
     dropFunction(document, function(buf) {
 	var  iccTableContainer= document.getElementById("iccTableContainer");
 	resetHTMLTable(iccTableContainer);
@@ -85,18 +98,36 @@ function main() {
 	addHTMLTable(iccTableContainer, "Header", header, "borderRed");
 	var foundTagTable = {};
 	var doneFigureTable = {};
+        var tristimulus = [null, null, null];
 	for (var idx in tagTable) {
 	    var tag = tagTable[idx];
 	    var signature = tag['Signature'];
+	    var type = tag['Type'];
 	    foundTagTable[signature] = true;
 	    var tagDetail = icc.getTagDetail(tag);
-	    switch (tag["Type"]) {
+	    switch (type) {
 	    case "desc":
 	    case "text":
 	    case "XYZ ":
-		var captionText = tag['Signature'];
+		var captionText = signature;
 		addHTMLTable(iccTableContainer, captionText, tagDetail, "borderGreen");
 		break;
+	    }
+	    function iccXYZ2yx(iccXYZ) {
+		return XYZ2xy([iccXYZ['XYZ']['X'], iccXYZ['XYZ']['Y'], iccXYZ['XYZ']['Z']]);
+	    }
+	    if (type === "XYZ ") {
+		switch (signature) {
+		case 'rXYZ':
+		    tristimulus[0] = iccXYZ2yx(tagDetail);
+		    break;
+		case 'gXYZ':
+		    tristimulus[1] = iccXYZ2yx(tagDetail);
+			break;
+		case 'bXYZ':
+		    tristimulus[2] = iccXYZ2yx(tagDetail);
+		    break;
+		}
 	    }
 	    if (foundTagTable['rXYZ'] && foundTagTable['gXYZ'] && foundTagTable['bXYZ']) {
 		if (! doneFigureTable['CIEDiagramRGB']) {
@@ -106,7 +137,9 @@ function main() {
 		    diagramBaseCanvas.setAttribute('class', "borderBlue");
 		    diagramBaseCanvas.width = 256;
 		    diagramBaseCanvas.height = 256;
-		    //iccTableContainer.appendChild(diagramBaseCanvas);
+		    iccTableContainer.appendChild(diagramBaseCanvas);
+		    params['tristimulus'] = tristimulus;
+		    drawDiagramBase(diagramBaseCanvas, params, true);
 		}
 	    }
 	    if (doneFigureTable['CIEDiagramRGB'] && foundTagTable['wtpt']) {
