887bc92d0ae6f4961d96b637945f649765805e4f
Yoshihiro Yamazaki <yoya@awm.jp>
2018-03-19 03:36:52 +0900

    srcMatrix0 button & text

--- matmulti.js
+++ matmulti.js
@@ -27,6 +27,10 @@ function main() {
     var srcMatrix1Table = document.getElementById("srcMatrix1Table");
     var srcMatrix2Table = document.getElementById("srcMatrix2Table");
     var dstMatrixTable = document.getElementById("dstMatrixTable");
+    var srcMatrixWindow0UpButton = document.getElementById("srcMatrixWindow0UpButton");
+    var srcMatrixWindow0Text = document.getElementById("srcMatrixWindow0Text");
+    var srcMatrixWindow0DownButton = document.getElementById("srcMatrixWindow0DownButton");
+
     var srcMatrix1 = [1, 0, 0,
 		      0, 1, 0,
 		      0, 0, 1];
@@ -48,6 +52,30 @@ function main() {
 	updateDstMatrix(srcMatrix1, srcMatrix1Window, srcMatrix2, srcMatrix2Window);
     }, srcMatrix2, srcMatrix2Window);
     updateDstMatrix(srcMatrix1, srcMatrix1Window, srcMatrix2, srcMatrix2Window);
+    bindFunction({ "srcMatrixWindow0DownButton":null,
+		   "srcMatrixWindow0UpButton":null,
+		   "srcMatrixWindow0Text":null },
+		 function(target, rel) {
+		     var id = target.id;
+		     var currentWindow0 = srcMatrix1.length / srcMatrix1Window;
+		     var newWindow0 = currentWindow0;
+		     if (id === "srcMatrixWindow0DownButton") {
+			 newWindow0--;
+		     } else if (id === "srcMatrixWindow0UpButton") {
+			 newWindow0++;
+		     } else {
+			 newWindow0 = parseFloat(srcMatrixWindow0Text.value);
+		     }
+		     if (newWindow0 != currentWindow0) {
+			 srcMatrix1 = extentMatrix(srcMatrix1, srcMatrix1Window, srcMatrix1Window, srcMatrix1Window*newWindow0);
+			 bindTableFunction("srcMatrix1Table", function(table, values, width) {
+			     srcMatrix1 = getTableValues(table.id);
+			     updateDstMatrix(srcMatrix1, srcMatrix1Window, srcMatrix2, srcMatrix2Window);
+			 }, srcMatrix1, srcMatrix1Window);
+			 updateDstMatrix(srcMatrix1, srcMatrix1Window, srcMatrix2, srcMatrix2Window);
+		     }
+
+		 });
 }
 
 function matrixMultiply(srcMatrix1, srcMatrix1Window, srcMatrix2, srcMatrix2Window) {
