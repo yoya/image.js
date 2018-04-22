543fa4bda8a1bd31ec1204604ab66da229c499e5
Yoshihiro Yamazaki <yoya@awm.jp>
2018-04-23 03:10:08 +0900

    remove scaled checkbox. always scaled on.

--- sigmoid.js
+++ sigmoid.js
@@ -22,24 +22,25 @@ function main() {
     }, "DataURL");
     bindFunction({"maxWidthHeightRange":"maxWidthHeightText",
 		  "aRange":"aText", "bRange":"bText",
-		  "scaledCheckbox":null, "linearCheckbox":null},
+		  "linearCheckbox":null},
 		 function(target, rel) {
 		     // console.debug(target.id);
 		     drawSrcImageAndSigmoid(srcImage, srcCanvas, dstCanvas, gammaCanvas, rel);
 		 } );
+    drawSrcImageAndSigmoid(srcImage, srcCanvas, dstCanvas, gammaCanvas, true);
 }
+
 function drawSrcImageAndSigmoid(srcImage, srcCanvas, dstCancas, gammaCanvas, sync) {
     var maxWidthHeight = parseFloat(document.getElementById("maxWidthHeightRange").value);
     var a = parseFloat(document.getElementById("aRange").value);
     var b = parseFloat(document.getElementById("bRange").value);
-    var scaled = document.getElementById("scaledCheckbox").checked;
     var linear = document.getElementById("linearCheckbox").checked;
     drawSrcImage(srcImage, srcCanvas, maxWidthHeight);
-    drawSigmoidGraph(gammaCanvas, a, b, scaled);
-    drawSigmoidImage(srcCanvas, dstCanvas, a, b, scaled, linear, sync);
+    drawSigmoidGraph(gammaCanvas, a, b);
+    drawSigmoidImage(srcCanvas, dstCanvas, a, b, linear, sync);
 }
 
-function drawSigmoidGraph(gammaCanvas, a, b, scaled) {
+function drawSigmoidGraph(gammaCanvas, a, b) {
     var ctx = gammaCanvas.getContext("2d");
     ctx.fillStyle="black";
     ctx.fillRect(0, 0, 256, 256);
@@ -48,16 +49,11 @@ function drawSigmoidGraph(gammaCanvas, a, b, scaled) {
     ctx.moveTo(256, 0)
     ctx.lineTo(256, 256);
     ctx.lineTo(0, 256);
-    if (scaled) {
-	var sig0 = sigmoid(0, a, b);
-	var sig1 = sigmoid(1, a, b);
-    }
+    var sig0 = sigmoid(0, a, b);
+    var sig1 = sigmoid(1, a, b);
     for (var x = 0 ; x < 256 ; x++) {
 	var v1 = x / 255;
-	var v2 = sigmoid(v1, a, b);
-	if (scaled) {
-	    v2 = (v2  - sig0) / (sig1 - sig0);
-	}
+	var v2 = (sigmoid(v1, a, b) - sig0) / (sig1 - sig0);
 	var y = (1 - v2) * 255;
 	ctx.lineTo(x, y);
     }
@@ -67,8 +63,8 @@ function drawSigmoidGraph(gammaCanvas, a, b, scaled) {
 
 var worker = new workerProcess("worker/sigmoid.js");
 
-function drawSigmoidImage(srcCanvas, dstCanvas, a, b, scaled, linear, sync) {
+function drawSigmoidImage(srcCanvas, dstCanvas, a, b, linear, sync) {
     // console.debug("drawSigmoidImage");
-    var params = { a:a, b:b, scaled:scaled, linear:linear };
+    var params = { a:a, b:b, linear:linear };
     worker.process(srcCanvas, dstCanvas, params, sync);
 }
--- worker/sigmoid.js
+++ worker/sigmoid.js
@@ -10,14 +10,15 @@ onmessage = function(e) {
     var srcImageData = e.data.image;
     var a = e.data.a;
     var b = e.data.b;
-    var scaled = e.data.scaled;
     var linear = e.data.linear;
     var width = srcImageData.width, height = srcImageData.height;
     var dstImageData = new ImageData(width, height);
     //
-    if (scaled) {
-	var sig0 = sigmoid(0.0, a, b);
-	var sig1 = sigmoid(1.0, a, b);
+    var sig0 = sigmoid(0.0, a, b);
+    var sig1 = sigmoid(1.0, a, b);
+    if (a < 0.001) {
+	postMessage({image:srcImageData}, [srcImageData.data.buffer]);
+	return ;
     }
     for (var y = 0 ; y < height; y++) {
         for (var x = 0 ; x < width; x++) {
@@ -27,21 +28,14 @@ onmessage = function(e) {
 	    } else {
 		rr /= 255;  gg /= 255;  bb /= 255;
 	    }
-	    rr = sigmoid(rr, a, b);
-	    gg = sigmoid(gg, a, b);
-	    bb = sigmoid(bb, a, b);
-	    // console.log([rr,gg,bb, aa])
-	    if (scaled) {
-		rr = (rr  - sig0) / (sig1 - sig0);
-		gg = (gg  - sig0) / (sig1 - sig0);
-		bb = (bb  - sig0) / (sig1 - sig0);
-	    }
+	    rr = (sigmoid(rr, a, b) - sig0) / (sig1 - sig0);
+	    gg = (sigmoid(gg, a, b) - sig0) / (sig1 - sig0);
+	    bb = (sigmoid(bb, a, b) - sig0) / (sig1 - sig0);
 	    if (linear) {
-		[rr, gg, bb] = linearRGB2sRGB([rr,gg,bb]);
+		[rr, gg, bb] = linearRGB2sRGB([rr, gg, bb]);
 	    } else {
-		rr *= 255;  gg *= 255;  b *= 255;
+		rr *= 255;  gg *= 255;  bb *= 255;
 	    }
-	    
 	    setRGBA(dstImageData, x, y, [rr, gg, bb, aa]);
 	}
     }
