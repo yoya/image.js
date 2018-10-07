8c97312a8e3a830c3d41ef4886b4ba6bf8e911ab
Yoshihiro Yamazaki <yoya@awm.jp>
2018-10-08 01:08:19 +0900

    add revRed, revGreen, revBlue checkbox.

--- czp.js
+++ czp.js
@@ -12,15 +12,27 @@ function main() {
     var canvas = document.getElementById("canvas")
     var widthRange  = document.getElementById("widthRange");
     var heightRange = document.getElementById("heightRange");
+    var revRedCheckbox = document.getElementById("revRedCheckbox");
+    var revGreenCheckbox = document.getElementById("revGreenCheckbox");
+    var revBlueCheckbox = document.getElementById("revBlueCheckbox");
     var params = {
 	"width":  parseFloat(widthRange.value),
-	"height": parseFloat(heightRange.value)
+	"height": parseFloat(heightRange.value),
+	"revRed":  revRedCheckbox.checked,
+	"revGreen":revGreenCheckbox.checked,
+	"revBlue": revBlueCheckbox.checked
     };
     bindFunction({"widthRange":"widthText",
-		  "heightRange":"heightText"},
+		  "heightRange":"heightText",
+		  "revRedCheckbox":null,
+		  "revGreenCheckbox":null,
+		  "revBlueCheckbox":null},
 		 function() {
 		     params["width"]  = parseFloat(widthRange.value);
 		     params["height"] = parseFloat(heightRange.value);
+		     params["revRed"]   = revRedCheckbox.checked;
+		     params["revGreen"] = revGreenCheckbox.checked;
+		     params["revBlue"]  = revBlueCheckbox.checked;
 		     drawCZP(canvas, params);
 		 } );
     drawCZP(canvas, params);
@@ -32,6 +44,9 @@ function drawCZP(canvas, params) {
     var ctx = canvas.getContext("2d");
     var width  = params.width;
     var height = params.height;
+    var revRed   = params.revRed;
+    var revGreen = params.revGreen;
+    var revBlue  = params.revBlue;
     canvas.width  = width;
     canvas.height = height;
     //
@@ -44,7 +59,10 @@ function drawCZP(canvas, params) {
 	    var yy = (y - height/2);
 	    var v = 128 * Math.sin(cx*xx*xx + cy*yy*yy) + 128;
 	    var rgba = [v, v, v, 255];
-	    setRGBA(imageData, x, y, rgba);
+	    var red   = (revRed)?(255-v):v;
+	    var green = (revGreen)?(255-v):v;
+	    var blue  = (revBlue)?(255-v):v;
+	    setRGBA(imageData, x, y, [red, green, blue, 255]);
 	}
     }
     ctx.putImageData(imageData, 0, 0);
