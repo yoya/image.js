7e068646f69831901c9243078c8e92b0cd7493a3
Yoshihiro Yamazaki <yoya@awm.jp>
2019-04-28 12:19:09 +0900

    usage) php csv2json_SB10.php SB10_corrected_indiv_CMFs.csv > SB10_corrected_indiv_CMFs.csv.json

---
+++ data/csv2json_SB10.php
@@ -0,0 +1,34 @@
+<?php
+
+$table = [];
+foreach (file($argv[1]) as $line) {
+    $table []= explode(',', trim($line));
+}
+
+echo "[".PHP_EOL;
+for ($x = 2; $x < count($table[0]) ; $x += 3) {
+    echo " {".PHP_EOL;
+    $Number = $table[0][$x];
+    $ObserverID = $table[1][$x];
+    $Sex = $table[2][$x];
+    $Age = $table[3][$x];
+    $Conditions = $table[4][$x];
+    $Name = $table[5][$x];
+    echo "  Number:$Number, ObserverID:'$ObserverID', Sex:'$Sex', Age:$Age, Conditions:'$Conditions', Name,'$Name',".PHP_EOL;
+    echo "  WRGB:[".PHP_EOL;
+    for ($y = 7 ; $y < count($table) ; $y++) {
+        $W = $table[$y][1];
+        $R = $table[$y][$x];
+        $G = $table[$y][$x+1];
+        $B = $table[$y][$x+2];
+        if (($W != "") && ($R != "") && ($G != "") && ($B != "")) {
+            echo "    [$W, $R, $G, $B],".PHP_EOL;            
+        } else {
+            if (($R != "") || ($G != "") || ($B != "")) {
+                fprintf(STDERR, "Error: W:'$W', R:'$R', G:'$G', B:'$B'".PHP_EOL);                
+            }
+        }
+    }
+    echo " }".PHP_EOL;
+}
+echo "]".PHP_EOL;
