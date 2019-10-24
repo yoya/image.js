2a3a26cb2be67bdb8f0b3488bc707ba3c1677bb7
Yoshihiro Yamazaki <yoya@awm.jp>
2019-10-25 00:39:34 +0900

    add max, min functions to Statistics

--- tests/statictics.js
+++ tests/statictics.js
@@ -6,7 +6,9 @@ var ave = Statistics.average(arr);
 var ave2 = Statistics.average(arr2);
 var vari = Statistics.variance(arr, ave);
 var [vari1, vari2, cov] = Statistics.variance_covariance(arr, arr2, ave, ave2);
-var [max, min] =  Statistics.max_min(arr);
+var max =  Statistics.max(arr);
+var min =  Statistics.min(arr);
+var [max2, min2] =  Statistics.max_min(arr);
 var arrNorm =  Statistics.normalize(arr, 100);
 var arrNorm2 =  Statistics.normalize(arr2, 100);
 
@@ -14,5 +16,5 @@ console.log("arr:", arr);
 console.log("ave:", ave);
 console.log("vari:", vari);
 console.log("vari1, vari2, cov:",  vari1,  vari2, cov);
-console.log("max, min:", max, min);
+console.log("max, min:", max, min, max2, min2);
 console.log("arrNorm", arr, arrNorm, arrNorm2);
