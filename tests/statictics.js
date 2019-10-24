69d8204351e11e16c58285174caa6e495a5129ab
Yoshihiro Yamazaki <yoya@awm.jp>
2019-10-24 23:54:29 +0900

    fix convariance.

--- tests/statictics.js
+++ tests/statictics.js
@@ -3,12 +3,16 @@ var Statistics = require("../lib/statistics")
 var arr = [1, 2, 3, 4];
 var arr2 = [7, 8, 9, 10];
 var ave = Statistics.average(arr);
+var ave2 = Statistics.average(arr2);
 var vari = Statistics.variance(arr, ave);
-var [vari1, vari2, cov] = Statistics.variance_covariance(arr, arr2, ave);
+var [vari1, vari2, cov] = Statistics.variance_covariance(arr, arr2, ave, ave2);
 var [max, min] =  Statistics.max_min(arr);
+var arrNorm =  Statistics.normalize(arr, 100);
+var arrNorm2 =  Statistics.normalize(arr2, 100);
 
 console.log("arr:", arr);
 console.log("ave:", ave);
 console.log("vari:", vari);
 console.log("vari1, vari2, cov:",  vari1,  vari2, cov);
 console.log("max, min:", max, min);
+console.log("arrNorm", arr, arrNorm, arrNorm2);
