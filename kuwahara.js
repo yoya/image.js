235bb961c4c1db9e2403e109f8a3e86923a5c966
Yoshihiro Yamazaki <yoya@awm.jp>
2020-06-12 23:51:19 +0900

    kuwahara filter.

---
+++ kuwahara.js
@@ -0,0 +1,54 @@
+"use strict";
+/*
+ * 2020/06/12- (c) yoya@awm.jp
+ * ref) https://qiita.com/Cartelet/items/5c1c012c132be3aa9608
+ */
+
+document.addEventListener("DOMContentLoaded", function(event) {
+    main();
+});
+
+function main() {
+    // console.debug("main");
+    var srcCanvas = document.getElementById("srcCanvas");
+    var varCanvas = document.getElementById("varCanvas");
+    var dstCanvas = document.getElementById("dstCanvas");
+    var dstCanvases = [varCanvas, dstCanvas];
+    var srcImage = new Image(srcCanvas.width, srcCanvas.height);
+    var params = {};
+    //
+    dropFunction(document, function(dataURL) {
+	srcImage = new Image();
+	srcImage.onload = function() {
+            let maxWidthHeight = params["maxWidthHeightRange"];
+	    drawSrcImage(srcImage, srcCanvas, maxWidthHeight);
+	    drawKuwaharaFilter(srcCanvas, dstCanvases, params, true);
+	}
+	srcImage.src = dataURL;
+    }, "DataURL");
+    bindFunction({"variWindowRange":"variWindowText",
+                  "vslideWindowRange":"vslideWindowText",
+                  "filterWindowRange":"filterWindowText"},
+		 function(target, rel) {
+		     drawKuwaharaFilter(srcCanvas, dstCanvases, params, rel);
+		 }, params);
+    bindFunction({"maxWidthHeightRange":"maxWidthHeightText"},
+		 function(target, rel) {
+		     let maxWidthHeight = params["maxWidthHeightRange"];
+		     drawSrcImage(srcImage, srcCanvas, maxWidthHeight);
+		     drawKuwaharaFilter(srcCanvas, dstCanvases, params, rel);
+		 }, params);
+}
+
+var worker = new workerProcess("worker/kuwahara.js");
+
+function drawKuwaharaFilter(srcCanvas, dstCanvases, params, sync) {
+    var params_w = {
+        rankOrder   : 1,
+        variWindow: params["variWindowRange"],
+        slideWindow: params["slideWindowRange"],
+        filterWindow: params["filterWindowRange"],
+    };
+    // console.debug("worker.process", srcCanvas, dstCanvases, params_w, sync);
+    worker.process(srcCanvas, dstCanvases, params_w, sync);
+}
---
+++ worker/kuwahara.js
@@ -0,0 +1,88 @@
+"use strict";
+/*
+ * 2020/06/12- (c) yoya@awm.jp
+ */
+
+importScripts("../lib/canvas.js");
+
+onmessage = function(e) {
+    var srcImageData = e.data.image;
+    // var [variWindow, slideWindow, filterWindow] = e.data;
+    var [variWindow, slideWindow, filterWindow] =
+        [e.data.variWindow, e.data.slideWindow, e.data.filterWindow];
+    var width = srcImageData.width, height = srcImageData.height;
+    var variImageData = new ImageData(width, height); // variance
+    var dstImageData  = new ImageData(width, height);
+    //
+    var variTable = makeVarianceTable(srcImageData, variWindow);
+    drawVarianceTable(variImageData, variTable);
+    //
+    for (var y = 0 ; y < (height - filterWindow + 1); y++) {
+        for (var x = 0 ; x < (width - filterWindow + 1); x++) {
+	    var rgba = kuwaharaFilter(srcImageData, variTable,
+                                      x, y, slideWindow, filterWindow);
+	    setRGBA(dstImageData, x, y, rgba);
+	}
+    }
+    postMessage({image:[variImageData, dstImageData]},
+                [variImageData.data.buffer, dstImageData.data.buffer]);
+}
+
+function makeVarianceTable(srcImageData, variWindow) {
+    let width = srcImageData.width, height = srcImageData.height;
+    let nSamples = width * height;
+    let variSamples = variWindow * variWindow;
+    let varianceTable = new Float32Array(nSamples);
+    let meansRGB = new Float32Array(3);
+    let variRGB  = new Float32Array(3);
+    for (let y = 0; y < (height - variWindow + 1); y++) {
+        for (let x = 0; x < (width - variWindow + 1); x++) {
+            meansRGB[0] = meansRGB[1] = meansRGB[2] = 0;
+            for (let yy = 0; yy < variWindow; yy++) {
+                for (let xx = 0; xx < variWindow; xx++) {
+                    let rgba = getRGBA(srcImageData, x+xx, y+yy);
+                    meansRGB[0] += rgba[0];
+                    meansRGB[1] += rgba[1];
+                    meansRGB[2] += rgba[2];
+                }
+            }
+            meansRGB[0] /= variSamples;
+            meansRGB[1] /= variSamples;
+            meansRGB[2] /= variSamples;
+            variRGB[0] = variRGB[1] = variRGB[2] = 0;
+            for (let yy = 0; yy < variWindow; yy++) {
+                for (let xx = 0; xx < variWindow; xx++) {
+                    let rgba = getRGBA(srcImageData, x+xx, y+yy);
+                    variRGB[0] += (rgba[0] - meansRGB[0]) ** 2;
+                    variRGB[1] += (rgba[1] - meansRGB[1]) ** 2;
+                    variRGB[2] += (rgba[2] - meansRGB[2]) ** 2;
+                }
+            }
+            let i = x + y * width;
+            varianceTable[i] = variRGB[0] + variRGB[1] + variRGB[2];
+        }
+    }
+    return varianceTable;
+}
+
+function drawVarianceTable(variImageData, variableTable) {
+    var min = variableTable.reduce((a,b) => (a<b)?a:b);
+    var max = variableTable.reduce((a,b) => (a>b)?a:b);
+    var scale = 255 * (max - min);
+    let n = variableTable.length;
+    for (var i = 0; i < n; i++) {
+        var v = variableTable[i];
+        v = ((v - min) * scale) | 0;
+        variImageData.data.set([v, v, v, 255], i*4);
+    }
+}
+    
+
+function kuwaharaFilter(srcImageData, variTable,
+                        x, y, slideWindow, filterWindow) {
+    let rgba = getRGBA(srcImageData, x, y);
+    return rgba;
+}
+
+
+
