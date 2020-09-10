b427661f184b70174a50a4c295bca3986d3ced9b
Yoshihiro Yamazaki <yoya@awm.jp>
2020-09-10 22:48:43 +0900

    add markerCheckbox.

--- homography.js
+++ homography.js
@@ -25,6 +25,7 @@ function main() {
     let dstCanvas = document.getElementById("dstCanvas");
     var srcImage = new Image(srcCanvas.width, srcCanvas.height);
     let maxWidthHeightRange = document.getElementById("maxWidthHeightRange");
+    let markerCheckbox = document.getElementById("markerCheckbox");
     var params = {
         maxWidthHeight: parseFloat(maxWidthHeightRange.value),
         coeff: [1, 0, 0,
@@ -39,14 +40,14 @@ function main() {
 	srcImage.src = dataURL;
     }, "DataURL");
     bindFunction({"maxWidthHeightRange":"maxWidthHeightText",
+                  "markerCheckbox":null,
                   "aRange":"aText", "bRange":"bText", "cRange":"cText",
                   "dRange":"dText", "eRange":"eText", "fRange":"fText",
                   "gRange":"gText", "hRange":"hText"},
 		 function(target, rel) {
-                     if (target.id === "maxWidthHeightRange" ||
-                         target.id === "maxWidthHeightText") {
-                         params["maxWidthHeight"] = parseFloat(maxWidthHeightRange.value);
-                     }
+                     params["maxWidthHeight"] = parseFloat(maxWidthHeightRange.value);
+                     params["marker"] = markerCheckbox.checked;
+
                      let num = coeffNameNumber(target.id);
                      if (num >= 0) {
                          params.coeff[num] = parseFloat(target.value);
@@ -58,12 +59,42 @@ function main() {
 var worker = new workerProcess("worker/homography.js");
 
 function drawHomograpy(srcImage, srcCanvas, dstCanvas, params, sync) {
-    var maxWidthHeight = params["maxWidthHeight"];
-    drawSrcImage(srcImage, srcCanvas, maxWidthHeight);
+    drawSrcImage(srcImage, srcCanvas, params["maxWidthHeight"]);
     //
-    var params_w = {
-        coeff: params.coeff
-    };
-    worker.process(srcCanvas, dstCanvas, params_w, sync);
+    worker.process(srcCanvas, dstCanvas, params, sync);
+    worker.addListener(function() {
+        if (params.marker) {
+            drawMarker(srcCanvas, params.coeff);
+        }
+    });
+}
+
+function drawMarker(canvas, coeff) {
+    let ctx = canvas.getContext("2d");
+    let width = canvas.width, height = canvas.height;
+    let xyArr = [
+        homography(0.0, 0.0, coeff), homography(1.0, 0.0, coeff),
+        homography(1.0, 1.0, coeff), homography(0.0, 1.0, coeff),
+    ];
+    for (let i = 0, n = xyArr.length; i < n; i++) {
+        let [x, y] = xyArr[i];
+        xyArr[i] = [x * width, y * height];
+    }
+    ctx.beginPath();
+    ctx.moveTo(xyArr[3][0],xyArr[3][1]);
+    let colors = [["red"], "yellow", "green", "blue"];
+    for (let i = 0, n = xyArr.length; i < n; i++) {
+        let [x, y] = xyArr[i];
+        ctx.strokeStyle = colors[(i+3)%4];
+        ctx.lineTo(x, y)
+        ctx.stroke();
+        ctx.beginPath();
+        ctx.strokeStyle = colors[i];
+        ctx.fillStyle = "white";
+        ctx.arc(x, y, 6, 0, 2*Math.PI, false);
+        ctx.stroke();
+        ctx.fill();
+        ctx.moveTo(x, y);
+    }
 }
 
