3ed6dd7cca0c6cf250188d7bcdb5a10fa1c974c0
Yoshihiro Yamazaki <yoya@awm.jp>
2021-03-03 01:50:28 +0900

    more show filter

--- showa.js
+++ showa.js
@@ -31,7 +31,7 @@ function drawSrcImageAndShowa(srcImage, srcCanvas, dstCancas) {
 }
 function colortrans_showa(r, g, b) {
     return [
-        0.8 * r + 0.1 * g + 0.2 * b,
+        0.7 * r + 0.1 * g + 0.2 * b,
         0.0 * r + 0.8 * g + 0.2 * b,
         0.1 * r + 0.2 * g + 0.6 * b
     ];
@@ -48,6 +48,37 @@ function contrast_showa(x) {
     return (2*x +1)/4 - Math.tan(1.1 - 2*x)/8;
 }
 
+function smoothing(srcImageData, srcX, srcY, filterMatrix, convWindow) {
+    var startX = srcX - (convWindow-1)/2, endX = startX + convWindow;
+    var startY = srcY - (convWindow-1)/2, endY = startY + convWindow;
+    var i = 0;
+    var [r2, g2, b2, a2] = [0,0,0,0];
+    for (var y = startY ; y < endY ; y++) {
+        for (var x = startX ; x < endX ; x++) {
+            var [r, g, b, a] = getRGBA(srcImageData, x, y, OUTFILL_EDGE);
+            r2 += r * filterMatrix[i];
+            g2 += g * filterMatrix[i];
+            b2 += b * filterMatrix[i];
+            i++;
+        }
+    }
+    var [r, g, b, a] = getRGBA(srcImageData, srcX, srcY);
+    return [r2, g2, b2, a];
+}
+
+function noize_showa(r, g, b) {
+    var r0 = Math.random()
+    var r1 = 0.2 * Math.random()
+    var rr = 0, rg = 0, rb = 0;
+    if (r0 < 0.25) {
+        rr = r1 * Math.random();
+    } else if (r0 < 0.75) {
+        rg = r1 * Math.random();
+    } else {
+        rb = r1 * Math.random();
+    }
+    return [ r - rr, g - rg, b - rb ];
+}
 
 function drawShowa(srcCanvas, dstCanvas) {
     console.debug("drawShowa");
@@ -60,18 +91,42 @@ function drawShowa(srcCanvas, dstCanvas) {
     dstCanvas.height = dstHeight;
     //
     var srcImageData = srcCtx.getImageData(0, 0, srcWidth, srcHeight);
+    var tmpImageData = srcCtx.getImageData(0, 0, srcWidth, srcHeight);
     var dstImageData = dstCtx.createImageData(dstWidth, dstHeight);
+
     for (var dstY = 0 ; dstY < dstHeight; dstY++) {
         for (var dstX = 0 ; dstX < dstWidth; dstX++) {
 	    var srcX = dstX;
 	    var srcY = dstY;
 	    var [r,g,b,a] = getRGBA(srcImageData, srcX, srcY);
             r /= 255 ;  g /= 255 ; b /= 255;
-            [r, g, b] = colortrans_showa(r, g, b)
             [r, g, b] = contrast_showa([r, g, b])
+            [r, g, b] = colortrans_showa(r, g, b)
+            [r, g, b] = noize_showa(r, g, b)
             r *= 255 ; g *= 255 ; b *= 255;
-	    setRGBA(dstImageData, dstX, dstY, [r,g,b,a]);
+	    setRGBA(tmpImageData, dstX, dstY, [r,g,b,a]);
 	}
     }
+    var filterWindow = 3;
+    var filterMatrix = new Float32Array(filterWindow * filterWindow);
+    var triangle = pascalTriangle(filterWindow);
+    var i = 0;
+    for (var y = 0; y < filterWindow; y++) {
+        for (var x = 0 ; x < filterWindow; x++) {
+            filterMatrix[i++] = triangle[x] * triangle[y];
+        }
+    }
+    var total = filterMatrix.reduce(function(p, v) {return p+v; });;
+    filterMatrix = filterMatrix.map(function(v) { return v / total; })
+    for (var dstY = 0 ; dstY < dstHeight; dstY++) {
+        for (var dstX = 0 ; dstX < dstWidth; dstX++) {
+	    var srcX = dstX;
+	    var srcY = dstY;
+            // var rgba = getRGBA(tmpImageData, srcX, srcY);
+            var rgba = smoothing(tmpImageData, srcX, srcY, filterMatrix, filterWindow);
+            setRGBA(dstImageData, dstX, dstY, rgba);
+        }
+    }    
+    
     dstCtx.putImageData(dstImageData, 0, 0);
 }
