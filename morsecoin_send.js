f505a10638c2b01fcf9a64190c1e215cfc68f31d
Yoshihiro Yamazaki <yoya@awm.jp>
2022-04-11 01:00:54 +0900

    send side point animation.

--- morsecoin_send.js
+++ morsecoin_send.js
@@ -30,9 +30,34 @@ function drawMorseSendPoint(canvas, a, color, idx) {
             return ;
         }
     }
-    const [c, x, y, ...m] = a;
+    if (color === undefined) { color = "#eeee00"; }
     const ctx = canvas.getContext("2d");
+    const {width, height} = canvas;
+    const [c, x0, y0, ...ma] = a;
     ctx.save();
+    ctx.fillStyle = color;
+    let x = x0 + 0.02;
+    let y = y0;
+    for (const i in ma) {
+        const m = ma[i];
+        ctx.beginPath();
+        if (m) { // morse long code
+            const xlen = 0.05;
+            const ylen = 0.01;
+            x += 0.005;
+            if ((idx === undefined) || (i == idx)) {
+                ctx.rect(x*width, (y-ylen/2)*height, width*xlen, height*ylen);
+            }
+            x += 0.055;
+        } else { // morse short code
+            x += 0.01;
+            if ((idx === undefined) || (i == idx)) {
+                ctx.arc(x*width, y*height, width*0.008, 0, 2*Math.PI);
+            }
+            x += 0.01;
+        }
+        ctx.fill();
+    }
     ctx.restore();
 }
 
@@ -79,24 +104,24 @@ const morseSendAlphabetList = [  // false:.  true:-
     ['H', 0.25, 0.75, false, false, false, false],  // ....
     ['I', 0.3, 0.8, false, false],  // ..
     //
-    ['J', 0.44, 0.27, false, true, true, true],  // .___
-    ['K', 0.42, 0.33, true, false, true],  // _._
-    ['L', 0.42, 0.39, false, true, false, false],  // ._..
-    ['M', 0.41, 0.45, true, true],  // __
-    ['N', 0.41, 0.51, true, false],  // _.
-    ['O', 0.40, 0.58, true, true, true],  // ___
-    ['P', 0.42, 0.64, false, true, true, false],  // .__.
-    ['Q', 0.43, 0.70, true, true, false, true],  // __._
-    ['R', 0.45, 0.76, false, true, false],  // ._.
-    ['S', 0.47, 0.83, false, false, false],  // ...
+    ['J', 0.42, 0.27, false, true, true, true],  // .___
+    ['K', 0.40, 0.33, true, false, true],  // _._
+    ['L', 0.40, 0.39, false, true, false, false],  // ._..
+    ['M', 0.39, 0.45, true, true],  // __
+    ['N', 0.39, 0.51, true, false],  // _.
+    ['O', 0.38, 0.57, true, true, true],  // ___
+    ['P', 0.40, 0.64, false, true, true, false],  // .__.
+    ['Q', 0.41, 0.70, true, true, false, true],  // __._
+    ['R', 0.43, 0.77, false, true, false],  // ._.
+    ['S', 0.45, 0.83, false, false, false],  // ...
     //
-    ['T', 0.62, 0.37, true],  // _
-    ['U', 0.62, 0.44, false, false, true],  // .._
-    ['V', 0.62, 0.51, false ,false, false, true],  // ..._
-    ['W', 0.62, 0.58, false, true, true],  // .__
-    ['X', 0.62, 0.65, true, false, false, true],  // _.._
-    ['Y', 0.62, 0.72, true, false, true, true],  // _.__
-    ['Z', 0.62, 0.79, true, true, false, false],  // __..
+    ['T', 0.62, 0.36, true],  // _
+    ['U', 0.62, 0.42, false, false, true],  // .._
+    ['V', 0.62, 0.48, false ,false, false, true],  // ..._
+    ['W', 0.62, 0.54, false, true, true],  // .__
+    ['X', 0.62, 0.61, true, false, false, true],  // _.._
+    ['Y', 0.62, 0.67, true, false, true, true],  // _.__
+    ['Z', 0.62, 0.74, true, true, false, false],  // __..
 ];
 
 
