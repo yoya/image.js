8d4a1f3c0dafe0f8b7c3fafa152b8cf127462327
Yoshihiro Yamazaki <yoya@awm.jp>
2022-08-30 02:32:59 +0900

    halftone

---
+++ halftone.js
@@ -0,0 +1,90 @@
+"use strict";
+/*
+ * 2022/08/28- (c) yoya@awm.jp
+ */
+
+document.addEventListener("DOMContentLoaded", function(event) {
+    main();
+});
+
+function main() {
+    // console.debug("main");
+    const srcCanvas = document.getElementById("srcCanvas");
+    const dstCanvas = document.getElementById("dstCanvas");
+    const srcImage = new Image();
+    const params = {};
+    srcImage.onload = function() {
+	drawSrcImageAndHalftone(srcImage, srcCanvas, dstCanvas, params);
+    }
+    srcImage.src = "./img/RGBCube.png"
+    dropFunction(document, function(dataURL) {
+	srcImage.src = dataURL;
+    }, "DataURL");
+    bindFunction({ "maxWidthHeightRange":"maxWidthHeightText",
+                   "scaleRange":"scaleText",
+                   "sizeRange":"sizeText",
+                   "rotateRange":"rotateText" },
+		 function() {
+		     drawSrcImageAndHalftone(srcImage, srcCanvas, dstCanvas,
+                                         params);
+		 }, params);
+}
+
+
+function drawSrcImageAndHalftone(srcImage, srcCanvas, dstCanvas, params) {
+    const scale = params.scaleRange;
+    const maxWidthHeight = Math.round(params.maxWidthHeightRange / scale);
+    drawSrcImage(srcImage, srcCanvas, maxWidthHeight);
+    rescaleCanvas(srcCanvas, scale);
+    drawHalftone(srcCanvas, dstCanvas, params);
+}
+
+function quantizeRound(value, size) {
+    return Math.round(value / size - 0.0000001) * size;
+}
+
+function quantizeCenter(x, y, size) {
+    return [ quantizeRound(x, size), quantizeRound(y, size) ];
+}
+
+function averageArea(imageData, x, y, size) {
+    return getRGBA(imageData, x, y, OUTFILL_EDGE);
+}
+
+function negateImage(canvas) {
+    const width = canvas.width, height = canvas.height;
+}
+
+function drawHalftone(srcCanvas, dstCanvas, params) {
+    console.debug("drawHalftone");
+    const size = params.sizeRange;
+    const rotate = params.rotateRange / 180 * Math.PI;
+    const srcCtx = srcCanvas.getContext("2d");
+    const dstCtx = dstCanvas.getContext("2d");
+    const width = srcCanvas.width, height = srcCanvas.height;
+    dstCanvas.width  = width;
+    dstCanvas.height = height;
+    //
+    const srcImageData = srcCtx.getImageData(0, 0, width, height);
+    const radius = size/2;
+    const margin = (1.414 - 1.0 ) / 2;
+    const xMin = - width * margin - size;
+    const xMax = width * (1+margin) + size;
+    const yMin = - height * margin - size;
+    const yMax = height * (1+margin) + size;
+    for (let y = yMin ; y < yMax; y += size) {
+        for (let x = xMin; x < xMax; x += size) {
+	    const rgba = averageArea(srcImageData,
+                                     Math.round(x), Math.round(y), size);
+            const hexColor = "#"+Utils.ToHexArray(rgba).join("");
+            dstCtx.beginPath();
+            dstCtx.fillStyle = hexColor;
+            dstCtx.translate(width / 2, height / 2);
+            dstCtx.rotate(rotate);
+            dstCtx.translate(-width / 2, -height / 2);
+            dstCtx.arc(x, y, radius, 0, Math.PI * 2, true);
+            dstCtx.fill();
+	}
+    }
+    
+}
