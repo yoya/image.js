0786353713160fe90973022070b86fc2de68024f
Yoshihiro Yamazaki <yoya@awm.jp>
2023-02-28 23:16:49 +0900

    - refine- - fix alpha value blend.

--- alphablend.js
+++ alphablend.js
@@ -9,30 +9,22 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
 function main() {
     // console.debug("main");
-    var srcCanvas1 = document.getElementById("srcCanvas1");
-    var srcCanvas2 = document.getElementById("srcCanvas2");
-    var srcCanvas1Container = document.getElementById("srcCanvas1Container");
-    var srcCanvas2Container = document.getElementById("srcCanvas2Container");
-    var dstCanvas = document.getElementById("dstCanvas");
-    var maxWidthHeight = parseFloat(document.getElementById("maxWidthHeightRange").value)
-    var linearGamma = document.getElementById("linearGammaCheckbox").checked;
-    var ratioRange  = document.getElementById("ratioRange");
-    var ratio1Range = document.getElementById("ratio1Range");
-    var ratio2Range = document.getElementById("ratio2Range");
-    var ratioText  = document.getElementById("ratioText");
-    var ratio1Text = document.getElementById("ratio1Text");
-    var ratio2Text = document.getElementById("ratio2Text");
+    const srcCanvas1 = document.getElementById("srcCanvas1");
+    const srcCanvas2 = document.getElementById("srcCanvas2");
+    const srcCanvas1Container = document.getElementById("srcCanvas1Container");
+    const srcCanvas2Container = document.getElementById("srcCanvas2Container");
+    const dstCanvas = document.getElementById("dstCanvas");
     //
-    var srcImage1 = new Image();
-    var srcImage2 = new Image();
+    const params = {};
+    const srcImage1 = new Image();
+    const srcImage2 = new Image();
     srcImage1.onload = function() {
-	drawSrcImage(srcImage1, srcCanvas1, maxWidthHeight);
-	drawAlphaBrend(srcCanvas1, srcCanvas2, dstCanvas);
+	drawSrcImage(srcImage1, srcCanvas1, params.maxWidthHeightRange);
+	drawAlphaBrend(srcCanvas1, srcCanvas2, dstCanvas, params, true);
     }
     srcImage2.onload = function() {
-	drawSrcImage(srcImage2, srcCanvas2, maxWidthHeight);
-	drawAlphaBrend(srcCanvas1, srcCanvas2, dstCanvas,
-		       linearGamma, true);
+	drawSrcImage(srcImage2, srcCanvas2, params.maxWidthHeightRange);
+	drawAlphaBrend(srcCanvas1, srcCanvas2, dstCanvas, params, true)
     }
     srcImage1.src = "img/4x4primary-400x400.png";
     srcImage2.src = "img/RGBCube.png";
@@ -49,46 +41,44 @@ function main() {
 		  "ratio1Range":"ratio1Text",
 		  "ratio2Range":"ratio2Text"},
 		 function(target, rel) {
-		     maxWidthHeight = parseFloat(document.getElementById("maxWidthHeightRange").value);
-		     linearGamma = document.getElementById("linearGammaCheckbox").checked;
+		     const maxWidthHeight = params.maxWidthHeightRange;
 		     if ((target.id === "ratioRange") || (target.id === "ratioText")) {
-			 var ratio = parseFloat(ratioRange.value);
-			 ratio1Range.value = ratio1Text.value = 1 - ratio;
-			 ratio2Range.value = ratio2Text.value = ratio;
+			 const ratio = params.ratioRange;
+                         params.ratio1Range = params.ratio1Text = 1 - ratio;
+			 params.ratio2Range = params.ratio2Text = ratio;
+                         bind2elements(params);
 		     }
 		     drawSrcImage(srcImage1, srcCanvas1, maxWidthHeight);
 		     drawSrcImage(srcImage2, srcCanvas2, maxWidthHeight);
-		     drawAlphaBrend(srcCanvas1, srcCanvas2, dstCanvas,
-				    linearGamma, rel);
-		 } );
+		     drawAlphaBrend(srcCanvas1, srcCanvas2, dstCanvas, params,
+				    rel);
+		 }, params );
     bindFunction({"methodSelect":null},
 		 function(target, rel) {
-		     drawAlphaBrend(srcCanvas1, srcCanvas2, dstCanvas,
-				    linearGamma, true);
-		 } );
+		     drawAlphaBrend(srcCanvas1, srcCanvas2, dstCanvas, params,
+				    true);
+		 }, params );
 }
 
-var  worker = new workerProcess("worker/alphablend.js");
+const worker = new workerProcess("worker/alphablend.js");
 
-function drawAlphaBrend(srcCanvas1, srcCanvas2, dstCanvas, linearGamma, sync) {
-    // console.debug("drawAlphaBrend")
-    var method = document.getElementById("methodSelect").value;
-    var ratio1 = parseFloat(document.getElementById("ratio1Range").value);
-    var ratio2 = parseFloat(document.getElementById("ratio2Range").value);
-    var srcCtx1 = srcCanvas1.getContext("2d");
-    var srcCtx2 = srcCanvas2.getContext("2d");
-    var dstCtx = dstCanvas.getContext("2d");
-    var srcWidth1 = srcCanvas1.width, srcHeight1 = srcCanvas1.height;
-    var srcWidth2 = srcCanvas2.width, srcHeight2 = srcCanvas2.height;
-    var dstWidth  = (srcWidth1  < srcWidth2) ? srcWidth1  : srcWidth2;
-    var dstHeight = (srcHeight1 < srcHeight2)? srcHeight1 : srcHeight2;
+function drawAlphaBrend(srcCanvas1, srcCanvas2, dstCanvas, params, sync) {
+    // console.debug("drawAlphaBrend", params);
+    const { methodSelect, linearGammaCheckbox, ratio1Range, ratio2Range } = params;
+    const srcCtx1 = srcCanvas1.getContext("2d");
+    const srcCtx2 = srcCanvas2.getContext("2d");
+    const dstCtx = dstCanvas.getContext("2d");
+    const srcWidth1 = srcCanvas1.width, srcHeight1 = srcCanvas1.height;
+    const srcWidth2 = srcCanvas2.width, srcHeight2 = srcCanvas2.height;
+    const dstWidth  = (srcWidth1  < srcWidth2) ? srcWidth1  : srcWidth2;
+    const dstHeight = (srcHeight1 < srcHeight2)? srcHeight1 : srcHeight2;
     dstCanvas.width  = dstWidth;
     dstCanvas.height = dstHeight;
     //
-    var srcImageData1 = srcCtx1.getImageData(0, 0, srcWidth1, srcHeight1);
-    var srcImageData2 = srcCtx2.getImageData(0, 0, srcWidth2, srcHeight2);
+    const srcImageData1 = srcCtx1.getImageData(0, 0, srcWidth1, srcHeight1);
+    const srcImageData2 = srcCtx2.getImageData(0, 0, srcWidth2, srcHeight2);
     //
-    var params = {method:method, ratio1:ratio1, ratio2:ratio2,
-		  linearGamma:linearGamma};
-    worker.process([srcCanvas1, srcCanvas2], dstCanvas, params, sync)
+    const params_w = {method:methodSelect, linearGamma:linearGammaCheckbox,
+                      ratio1:ratio1Range, ratio2:ratio2Range };
+    worker.process([srcCanvas1, srcCanvas2], dstCanvas, params_w, sync)
 }
--- worker/alphablend.js
+++ worker/alphablend.js
@@ -8,60 +8,58 @@ importScripts("../lib/color.js");
 importScripts("../lib/canvas.js");
 
 onmessage = function(e) {
-    var [srcImageData1, srcImageData2] = e.data.image;
-    var method = e.data.method;
-    var ratio1 = e.data.ratio1
-    var ratio2 = e.data.ratio2
-    var linearGamma = e.data.linearGamma;
-    var srcWidth1 = srcImageData1.width, srcHeight1 = srcImageData1.height;
-    var srcWidth2 = srcImageData2.width, srcHeight2 = srcImageData2.height;
-    var dstWidth  = (srcWidth1  < srcWidth2) ? srcWidth1  : srcWidth2;
-    var dstHeight = (srcHeight1 < srcHeight2)? srcHeight1 : srcHeight2;
+    const [srcImageData1, srcImageData2] = e.data.image;
+    const { method, ratio1, ratio2, linearGamma } = e.data;
+    const srcWidth1 = srcImageData1.width, srcHeight1 = srcImageData1.height;
+    const srcWidth2 = srcImageData2.width, srcHeight2 = srcImageData2.height;
+    const dstWidth  = (srcWidth1  < srcWidth2) ? srcWidth1  : srcWidth2;
+    const dstHeight = (srcHeight1 < srcHeight2)? srcHeight1 : srcHeight2;
 
-    var dstImageData = new ImageData(dstWidth, dstHeight);
+    const dstImageData = new ImageData(dstWidth, dstHeight);
 
-    for (var dstY = 0 ; dstY < dstHeight; dstY++) {
-        for (var dstX = 0 ; dstX < dstWidth; dstX++) {
-	    var srcX1 = dstX, srcY1 = dstY;
-	    var srcX2 = dstX, srcY2 = dstY;
-	    var rgba1 = getRGBA(srcImageData1, srcX1, srcY1);
-	    var rgba2 = getRGBA(srcImageData2, srcX2, srcY2);
+    for (let dstY = 0 ; dstY < dstHeight; dstY++) {
+        for (let dstX = 0 ; dstX < dstWidth; dstX++) {
+	    const srcX1 = dstX, srcY1 = dstY;
+	    const srcX2 = dstX, srcY2 = dstY;
+	    let rgba1 = getRGBA(srcImageData1, srcX1, srcY1);
+	    let rgba2 = getRGBA(srcImageData2, srcX2, srcY2);
+	    let r1,g1,b1,a1, r2,g2,b2,a2;
 	    if (linearGamma) {
 		rgba1 = sRGB2linearRGB(rgba1);
 		rgba2 = sRGB2linearRGB(rgba2);
-		var [r1,g1,b1,a1] = rgba1;
-		var [r2,g2,b2,a2] = rgba2;
+		[r1,g1,b1,a1] = rgba1;
+		[r2,g2,b2,a2] = rgba2;
 	    } else {
-		var [r1,g1,b1,a1] = rgba1; // uint to double
-		var [r2,g2,b2,a2] = rgba2; // uint to double
+		[r1,g1,b1,a1] = rgba1; // uint to double
+		[r2,g2,b2,a2] = rgba2; // uint to double
 		[r1, g1, b1, a1] = [r1, g1, b1, a1].map(function(v) { return v/255; });
 		[r2, g2, b2, a2] = [r2, g2, b2, a2].map(function(v) { return v/255; });
 	    }
-	    var rgba;
+	    let rgba;
 	    switch (method) {
 	    case "plus":
 		rgba = [r1*ratio1 + r2*ratio2,
 			g1*ratio1 + g2*ratio2,
 			b1*ratio1 + b2*ratio2,
-			(a1+a2)/2];
+			a1*ratio1 + a2*ratio2];
 		break;
 	    case "minus":
 		rgba = [r1*ratio1 - r2*ratio2,
 			g1*ratio1 - g2*ratio2,
 			b1*ratio1 - b2*ratio2,
-			(a1+a2)/2];
+                        a1*ratio1 - a2*ratio2];
 		break;
 	    case "multi":
 		rgba = [r1*ratio1 * r2*ratio2,
 			g1*ratio1 * g2*ratio2,
 			b1*ratio1 * b2*ratio2,
-			(a1 + a2)/2];
+                        a1*ratio1 * a2*ratio2];
 		break;
 	    case "div":
 		rgba = [r1*ratio1 / r2*ratio2,
 			g1*ratio1 / g2*ratio2,
 			b1*ratio1 / b2*ratio2,
-			(a1 + a2)/2];
+                        a1*ratio1 / a2*ratio2];
 		break;
 	    default:
 		console.error("unknown method:"+method);
