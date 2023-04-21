886fc923685feef867065d4644820d5ceff57c0d
Yoshihiro Yamazaki <yoya@awm.jp>
2023-04-22 01:01:29 +0900

    add APP1-Exif, APP2-ICC_Profile div element.

--- jpegmeta.js
+++ jpegmeta.js
@@ -8,59 +8,71 @@ document.addEventListener("DOMContentLoaded", function(event) {
     main();
 });
 
+image.onload = () => {
+    imageCaption.innerText = image.naturalWidth + "x" + image.naturalHeight;
+}
+
 function main() {
     dropFunction(document, function(arrbuf) {
-        var arr = new Uint8Array(arrbuf);
-        dump(arr);
-        imageContainer.innerHTML = "";
-        const blob = new Blob([arrbuf]);
         const reader = new FileReader();
-        reader.onload = function() {
-	    const url = this.result;
-            //const img = document.createElement("img");
-            const img = new Image();
-            img.onload = () => {
-                const sizeStr = img.naturalWidth + "x" + img.naturalHeight;
-                imageSize.innerText = sizeStr;
-                imageContainer.appendChild(img);
-            }
-            img.src = url;
+        reader.onload = () => {
+	    const url = reader.result;
+            image.src = url;
         }
+        const blob = new Blob([arrbuf]);
         reader.readAsDataURL(blob);
+        const arr = new Uint8Array(arrbuf);
+        dump(arr);
     }, "ArrayBuffer");
 }
 
 function dump(arr) {
-    let io = new IO_JPEG();
-    io.parse(arr);
-    console.log(arr, io);
-    const jfif = io.getJFIF();
-    const exif = io.getExif();
-    const iccdata = io.getICC();
-    const chunkList = io.getChunkList();
+    let jpeg = new IO_JPEG();
+    jpeg.parse(arr);
+    const jfif = jpeg.getJFIF();
+    const exif = jpeg.getExif();
+    const icc = jpeg.getICC();
+    const chunkList = jpeg.getChunkList();
+    console.debug( {chunkList });
     if (jfif) {
-        const items = [];
-        const [ver1, ver2, units, xd1, xd2, yd1, yd2, xThumb, yThumb] = jfif;
-        const version = ver1 + "." + Utils.LeftPad(ver2, 2, "0");
-        const xDensity = xd1 * 0xff  + xd2;
-        const yDensity = yd1 * 0xff  + yd2;
-        const td = [version, units, xDensity, yDensity, xThumb, yThumb];
-        items.push("JFIF version: " + version);
-        items.push("Density: " + xDensity +":" + yDensity + " ("+["aspect ratio", "inch", "cm"][units] + ")");
-        items.push("Thumbnail: " + xThumb + "x" + yThumb);
+        jfifCaption.innerText = "byte length:" + jfif.length;
+        const items = jfifFunction(jfif);
         makeItems(jfifContainer, items);
     } else {
-        makeItems(jfifContainer, ["JFIF not found"]);
+        jfifCaption.innerText = "APP0-JFIF not found";
+        makeItems(jfifContainer, []);
     }
     if (exif) {
+        exifCaption.innerText = "byte length:" + exif.length;
         console.log({ exif });
+    } else {
+        exifCaption.innerText = "APP1-Exif not found";
+    }
+    if (icc) {
+        iccCaption.innerText = "byte length:" + icc.length;
+        console.log({ icc });
+    } else {
+        iccCaption.innerText = "APP2-ICC_Profile not found";
     }
     console.debug(chunkList);
 }
 
+function jfifFunction(jfif) {
+    const items = [];
+    const [ver1, ver2, units, xd1, xd2, yd1, yd2, xThumb, yThumb] = jfif;
+    const version = ver1 + "." + Utils.LeftPad(ver2, 2, "0");
+    const xDensity = xd1 * 0xff  + xd2;
+    const yDensity = yd1 * 0xff  + yd2;
+    items.push("JFIF version: " + version);
+    const unitsStr = ["aspect ratio", "inch (DPI)", "cm"][units];
+    items.push("Density: " + xDensity +":" + yDensity + " (" + unitsStr + ")");
+    items.push("Thumbnail: " + xThumb + "x" + yThumb);
+    return items;
+}
+
 function makeItems(target, items) {
     target.innerHTML = "";
-    for (let item of items) {
+    for (const item of items) {
         const div = document.createElement("div");
         div.innerText = item;
         div.className = "leaf";
