d438ae0ccb2b6a1cf06155d5ef124de61b049943
Yoshihiro Yamazaki <yoya@awm.jp>
2023-07-21 14:09:46 +0900

    fix image loading. constraint max/min luminance order.

--- lb.js
+++ lb.js
@@ -11,10 +11,13 @@ function main() {
     // console.debug("main");
     const srcCanvas = document.getElementById("srcCanvas");
     const dstCanvas = document.getElementById("dstCanvas");
-    let srcImage = new Image();
+    const { width, height } = srcCanvas;
+    let srcImage = new Image(width, height);
     const params = {};
     dropFunction(document, function(dataURL) {
+        srcImage = new Image();
 	srcImage.onload = function() {
+            const { width, height } = srcCanvas;
 	    drawSrcImageAndCopy(srcImage, srcCanvas, dstCanvas, params, true);
 	}
 	srcImage.src = dataURL;
@@ -24,6 +27,25 @@ function main() {
                   "maxLuminance":"maxLuminanceText",
                   "minLuminance":"minLuminanceText"},
 		 function(target, rel) {
+                     switch (target.id) {
+                     case "maxLuminance":
+                         if (params.maxLuminance <= params.minLuminance) {
+                             const lumi = params.minLuminance + 1;
+                             params.maxLuminance = lumi;
+                             params.maxLuminanceText = lumi;
+                             maxLuminance.value =  lumi;
+                             maxLuminanceText.value =  lumi;
+                         }
+                         break;
+                     case "minLuminance":
+                         if (params.minLuminance >= params.maxLuminance) {
+                             const lumi = params.maxLuminance - 1;
+                             params.minLuminance = lumi;
+                             params.minLuminanceText = lumi;
+                             minLuminance.value = lumi;
+                             minLuminanceText.value = lumi;
+                         }
+                     }
 		     drawSrcImageAndCopy(srcImage, srcCanvas, dstCanvas, params, rel);
 		 }, params);
  }
