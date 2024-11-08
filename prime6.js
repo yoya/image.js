7dff18ef77810ad8327be69cec1a3d52033f81e8
Yoshihiro Yamazaki <yoya@awm.jp>
2024-11-08 20:17:55 +0900

    prime6 の 6 を変更可能に。

--- prime6.js
+++ prime6.js
@@ -11,7 +11,8 @@ function main() {
     // console.debug("main");
     const canvas = document.getElementById("canvas");
     const params = {};
-    bindFunction({"widthHeight":"widthHeightText"},
+    bindFunction({"widthHeight":"widthHeightText",
+                  "period":"periodText"},
 		 function() {
 		     drawPrime6(canvas, params);
 		 }, params);
@@ -28,10 +29,18 @@ function isPrime(n) {
     return true;
 }
 
+function drawText(ctx, x, y, text, color) {
+    ctx.save();
+    ctx.fillStyle = color;
+    ctx.textAlign = "center";
+    ctx.fillText(text, x, y);
+    ctx.restore();
+}
+
 function drawPrime6(canvas, params) {
     // console.debug("drawCopy");
     const ctx = canvas.getContext("2d");
-    const { widthHeight } = params;
+    const { widthHeight, period } = params;
     const width = widthHeight;
     const height = widthHeight;
     console.log(params, {width, height });
@@ -42,19 +51,21 @@ function drawPrime6(canvas, params) {
     const n = 500;
     const cx = width / 2;  // center
     const cy = height / 2;
-    const gap = 20;
+    const gap1 = period*1.5 + 12;
+
+    drawText(ctx, cx, cy, String(0), "gray");
     for (let i = 1 ; i < n; i++) {
-        const t = (i - 1) % 6;  // ６つのうち何個目(0数え)
-        const d = ((i + 5)/6) | 0;  // 中心から何番目(1数え)
-        const x = cx + gap * d * Math.sin(t / 6 * (2 * 3.14159));
-        const y = cy - gap * d * Math.cos(t / 6 * (2 * 3.14159));
-        ctx.save();
+        const t = (i - 1) % period;  // N(初期値:6)つのうち何個目(0数え)
+        const d = ((i - 1)/period) | 0;  // 中心から何番目(0数え)
+        const gap2 = 20;
+        const r = gap1 + gap2 * d 
+        const x = cx + r * Math.sin(t / period * (2 * 3.14159));
+        const y = cy - r * Math.cos(t / period * (2 * 3.14159));
         if (isPrime(i)) {
-            ctx.fillStyle = "#FE0";
+            drawText(ctx, x, y, String(i), "#FE0");
         } else {
-            ctx.fillStyle = "cyan";
+            drawText(ctx, x, y, String(i), "cyan");
         }
-        ctx.textAlign = "center";
-        ctx.fillText(String(i), x, y);
+        
     }
 }
